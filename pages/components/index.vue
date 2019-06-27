0ffc95e9cfa8e0d0d3c464d5adfb0526c1a78de7
zhaojintian <729234283@qq.com>
2019-06-27 10:44:15 +0800

    update

--- pages/components/index.vue
+++ pages/components/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="p-components">
-    components
+    <h2>按钮</h2>
+    <button class="c-button br8 w200 h60 bgc4 c1 fs32">
+      按钮
+    </button>
+    <h2>头像</h2>
+    <img
+      class="c-avatar"
+      src="https://mp-api.chongyangyanglao.com/static/components/icon/logo.png"
+    />
+    <h2>图标</h2>
+    <i class="c-icon c-icon--address"></i>
+    <h2>标签</h2>
+    <div class="c-tag h30 bdc5 c5 fs20">公司</div>
   </div>
 </template>
 
