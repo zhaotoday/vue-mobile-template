106036a806e9c6c37aacf4566c49f924cbbd7598
zhaojintian <729234283@qq.com>
2019-07-07 14:04:28 +0800

    update

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,7 +3,7 @@
     <div class="b-logo">
       <c-logo></c-logo>
     </div>
-    <h2 class="b-title fs32">福菜商城将获取以下授权：</h2>
+    <h2 class="b-title fs32">力软小程序将获取以下授权：</h2>
     <p class="b-desc c9 fs28">获得您的公开信息（昵称、头像等）</p>
     <button
       class="c-button w670 h76 bgc4 c1 fs32"
