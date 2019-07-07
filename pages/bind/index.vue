bf9c4b22e530fe4217896645f7585582e8aba445
zhaojintian <729234283@qq.com>
2019-07-07 18:12:22 +0800

    update

--- pages/bind/index.vue
+++ pages/bind/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="p-bind">
-    <div class="c-list bgc1 fs30 u-mt20">
+    <div class="c-list bgc1 fs30 u-mt16">
       <div class="c-list__item">
         <input
           class="c-list__value is-full fs32"
           type="number"
           maxlength="11"
-          placeholder-class="c9"
+          placeholder-class="c32"
           placeholder="请输入要绑定的手机号"
           v-model="cForm.phoneNumber" />
         <div
@@ -20,13 +20,13 @@
           class="c-list__value is-full fs32"
           type="number"
           maxlength="6"
-          placeholder-class="c9"
+          placeholder-class="c32"
           placeholder="请输入验证码"
           v-model="cForm.checkCode" />
       </div>
     </div>
     <button
-      class="c-button is-foot w670 h76 bgc4 c1 fs32"
+      class="c-button is-foot w650 h88 bgc21 c11 fs32"
       @click="confirm">
       提交
     </button>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -6,7 +6,7 @@
     <h2 class="b-title fs32">力软小程序将获取以下授权：</h2>
     <p class="b-desc c31 fs28">获得您的公开信息（昵称、头像等）</p>
     <button
-      class="c-button w670 h88 bgc21 c11 fs32"
+      class="c-button w650 h88 bgc21 c11 fs32"
       open-type="getUserInfo"
       @getuserinfo="getUserInfo">
       授权登录
