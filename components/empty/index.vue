b8cc15ea3e4ac27e5f23386c0018108ef3fe6876
zhaojintian <729234283@qq.com>
2019-07-07 15:34:13 +0800

    update

---
+++ components/empty/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="c-empty u-tac">
+    <i class="c-icon c-icon--empty"></i>
+    <p class="c-empty__tip c8 fs28">空空如也...</p>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style
+  lang="scss"
+  src="./style.scss">
+</style>
