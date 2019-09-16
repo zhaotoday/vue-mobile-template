82e2cd86eff8fce0f71e30870cb8a32c9a4aea77
zhaojintian <729234283@qq.com>
2019-09-16 15:48:09 +0800

    update

--- store/index.js
+++ store/index.js
@@ -24,17 +24,6 @@ export default new Vuex.Store({
   mutations,
   modules: {
     'public/wxUsers': require('./modules/public/wx-users').default,
-    'public/cartProducts': require('./modules/public/cart-products').default,
-    'public/articles': require('./modules/public/articles').default,
-    'public/natatoriums': require('./modules/public/natatoriums').default,
-    'public/students': require('./modules/public/students').default,
-    'public/trainers': require('./modules/public/trainers').default,
-    'public/products': require('./modules/public/products').default,
-    'wx/wxUsers': require('./modules/wx/wx-users').default,
-    'wx/payments': require('./modules/wx/payments').default,
-    'wx/orders': require('./modules/wx/orders').default,
-    'wx/products': require('./modules/wx/products').default,
-    'wx/studentProducts': require('./modules/wx/student-products').default,
-    'wx/studentProductComments': require('./modules/wx/student-product-comments').default
+    'wx/wxUsers': require('./modules/wx/wx-users').default
   }
 })
