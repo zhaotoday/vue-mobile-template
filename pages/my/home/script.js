239f57b3fe4bc6a608354e89baf8455770540584
zhaojintian <729234283@qq.com>
2019-09-19 03:32:51 +0800

    UPDATE

--- pages/my/home/script.js
+++ pages/my/home/script.js
@@ -1,22 +1,6 @@
 export default {
-  data () {
-    return {
-      user: {},
-      detail: {}
-    }
-  },
   async onShow () {
     await this.loggedIn()
     await this.phoneNumberBound()
-
-    this.detail = await this.getDetail()
-    this.user = this.$auth.get()['user']
-  },
-  methods: {
-    async getDetail () {
-      return this.$store.dispatch('wx/wxUsers/getDetail', {
-        id: this.$auth.get()['user'].id
-      })
-    }
   }
 }
