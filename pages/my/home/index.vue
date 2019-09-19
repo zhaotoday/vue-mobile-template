8dbfc6cf87caf023aa8d43610791b75ff2ffe6cc
zhaojintian <729234283@qq.com>
2019-09-19 16:25:29 +0800

    update

--- pages/my/home/index.vue
+++ pages/my/home/index.vue
@@ -1,9 +1,40 @@
 <template>
   <div
     v-if="user.id"
-    class="p-my-home">
-    {{ user.nickName }}
-    {{ user.phoneNumber }}
+    class="p-my">
+    <div class="b-bg"></div>
+    <img
+      class="c-avatar"
+      :src="user.avatarUrl"
+    />
+    <div class="b-name fs40 u-tac">{{ detail.name || user.nickName }}</div>
+    <div class="b-phone c35 fs28 u-tac">手机号：{{ user.phoneNumber }}</div>
+    <div class="c-list fs34">
+      <div
+        class="c-list__item has-icon is-link"
+        @click="navigateTo('/pages/my/info/index')">
+        <i class="c-icon c-icon--info"></i>
+        我的资料
+      </div>
+      <div
+        class="c-list__item has-icon is-link"
+        @click="navigateTo('/pages/orders/index')">
+        <i class="c-icon c-icon--order"></i>
+        我的订单
+      </div>
+      <div
+        class="c-list__item has-icon is-link"
+        @click="navigateTo('/pages/my/products/home/index')">
+        <i class="c-icon c-icon--product"></i>
+        我的课程
+      </div>
+      <button
+        class="c-list__item has-border is-button has-icon is-link bgc11 fs34"
+        open-type="contact">
+        <i class="c-icon c-icon--service"></i>
+        客户服务
+      </button>
+    </div>
   </div>
 </template>
 
