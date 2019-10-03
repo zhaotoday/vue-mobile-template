d89fddcc992a237962ee3f1ae9ff20699de393e5
zhaojintian <729234283@qq.com>
2019-10-03 08:33:14 +0800

    update

---
+++ components/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="c-index">
+    <div class="cc-list">
+      <div class="cc-list__title bg31 fs28">
+        A
+      </div>
+      <div
+        v-for="item in 8"
+        :key="item"
+        class="cc-list__item">
+        安顺
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
