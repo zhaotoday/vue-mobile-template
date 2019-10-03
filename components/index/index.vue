9e27750007c82fcb9042126ed8193b930d7db4c1
zhaojintian <729234283@qq.com>
2019-10-03 08:48:11 +0800

    update

--- components/index/index.vue
+++ components/index/index.vue
@@ -7,10 +7,18 @@
       <div
         v-for="item in 8"
         :key="item"
-        class="cc-list__item">
+        class="cc-list__item bgc11 fs26">
         安顺
       </div>
     </div>
+    <ul class="cc-letters">
+      <li
+        v-for="item in letters"
+        :key="item"
+        class="cc-letters__item">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
 
--- components/index/script.js
+++ components/index/script.js
@@ -1,1 +1,7 @@
-export default {}
+export default {
+  data () {
+    return {
+      letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    }
+  }
+}
