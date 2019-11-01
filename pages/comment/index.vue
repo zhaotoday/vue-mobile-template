52866cf4da8522e45de7646af36908a72f6f7475
zhaojintian <729234283@qq.com>
2019-11-02 06:15:14 +0800

    update

---
+++ pages/comment/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="p-join c-comment">
+    <h2 class="c-comment__title c12 fs30">所在城市</h2>
+    <input
+      class="c-comment__input fs28 bgc11"
+      placholder-class="c32"
+      placeholder="请输入所在城市"
+      v-model="cForm.city"
+    />
+    <h2 class="c-comment__title c12 fs30">您的姓名</h2>
+    <input
+      class="c-comment__input fs28 bgc11"
+      placholder-class="c32"
+      placeholder="请输入您的姓名"
+      v-model="cForm.name"
+    />
+    <h2 class="c-comment__title c12 fs30">手机号</h2>
+    <input
+      class="c-comment__input fs28 bgc11"
+      type="number"
+      maxlength="11"
+      placholder-class="c32"
+      placeholder="请输入手机号"
+      v-model="cForm.phoneNumber"
+    />
+    <h2 class="c-comment__title c12 fs30">留言建议（可以不填）</h2>
+    <textarea
+      class="c-comment__textarea fs28 bgc11"
+      placholder-class="c32"
+      placeholder="请输入留言建议"
+      v-model="cForm.suggestion">
+    </textarea>
+    <button
+      class="c-button is-foot w650 h88 bg1 c11 fs32"
+      @click="submit">
+      提交
+    </button>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style
+  lang="scss"
+  src="./style.scss">
+</style>
---
+++ pages/comment/script.js
@@ -0,0 +1,49 @@
+export default {
+  data () {
+    return {
+      cForm: {
+        city: '',
+        name: '',
+        phoneNumber: '',
+        suggestion: ''
+      }
+    }
+  },
+  methods: {
+    async submit () {
+      const PHONE_REG = /^1\d{2}\s?\d{4}\s?\d{4}$/
+      const { city, name, phoneNumber } = this.cForm
+
+      if (!city.trim()) {
+        this.$wx.showToast({ title: '请输入所在城市' })
+        return
+      }
+
+      if (!name.trim()) {
+        this.$wx.showToast({ title: '请输入您的姓名' })
+        return
+      }
+
+      if (!phoneNumber.trim()) {
+        this.$wx.showToast({ title: '请填写手机号' })
+        return
+      }
+
+      if (!PHONE_REG.test(phoneNumber)) {
+        this.$wx.showToast({ title: '手机号格式错误' })
+        return
+      }
+
+      await this.$store.dispatch('wx/joins/post', {
+        body: this.cForm
+      })
+
+      this.$wx.showToast({ title: '提交成功' })
+
+      await this.$helpers.sleep(1500)
+
+      this.$wx.navigateBack()
+    }
+  }
+}
+
