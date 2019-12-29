dbeefe866035d49e2f7c9f04cf1f19a351147f06
zhaojintian <729234283@qq.com>
2019-12-29 19:44:48 +0800

    update

--- pages/components/index.vue
+++ pages/components/index.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="p-components">
-    <c-search
-      placeholder="请输入..."
-      show-submit>
-    </c-search>
+    <h2>搜索框</h2>
+    <p>
+      <c-search
+        placeholder="请输入..."
+        show-submit>
+      </c-search>
+    </p>
     <h2>按钮</h2>
-    <button class="c-button br8 w200 h60 bgc4 c1 fs32">
-      按钮
-    </button>
+    <p>
+      <button class="c-button br8 w1 h88 bgc21 c11 fs32">
+        按钮
+      </button>
+    </p>
     <h2>头像</h2>
-    <img
-      class="c-avatar"
-      src="https://mp-api.chongyangyanglao.com/static/components/icon/logo.png"
-    />
+    <p>
+      <img
+        class="c-avatar"
+        src="https://mp-api.chongyangyanglao.com/static/components/icon/logo.png"
+      />
+    </p>
     <h2>图标</h2>
     <i class="c-icon c-icon--address"></i>
     <h2>标签</h2>
