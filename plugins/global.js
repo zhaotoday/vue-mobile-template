9e42e5a96be55c88023e683cfb07307958d1be2b
zhaojintian <729234283@qq.com>
2020-01-03 00:24:56 +0800

    update

--- plugins/global.js
+++ plugins/global.js
@@ -1,19 +1,19 @@
-import store from '@/store'
-import consts from '@/utils/consts'
-import helpers from '@/utils/helpers/base'
 import time from 'jt-time'
 import auth from 'we-design/utils/auth'
 import wxb from 'we-design/utils/wxb'
+import store from '@/store'
+import consts from '@/utils/consts'
+import helpers from '@/utils/helpers/base'
 import globalMixin from '@/mixins/global'
 
 export default {
   install (Vue) {
-    Vue.prototype.$store = store
-    Vue.prototype.$consts = consts
-    Vue.prototype.$helpers = helpers
     Vue.prototype.$time = time
     Vue.prototype.$auth = auth
     Vue.prototype.$wx = wxb
+    Vue.prototype.$store = store
+    Vue.prototype.$consts = consts
+    Vue.prototype.$helpers = helpers
 
     Vue.mixin(globalMixin)
   }
