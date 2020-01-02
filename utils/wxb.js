d78a10a7159713bdc44754b396324cc51e18c55a
zhaojintian <729234283@qq.com>
2020-01-02 22:38:27 +0800

    update

--- utils/wxb.js
+++ utils/wxb.js
@@ -1,27 +1,5 @@
 import wxb from 'wx-bridge'
-import consts from './consts'
 import helpers from './helpers/base'
-import auth from './auth'
-
-const navigateInterceptor = {
-  async beforeCall (options) {
-    if (options.requiresLogin && !auth.loggedIn()) {
-      options.url = consts.LOGIN_PAGE
-    }
-
-    if (options.requiresBind && !auth.phoneNumberBound()) {
-      options.url = consts.BIND_PAGE
-    }
-  }
-}
-
-wxb.navigateTo = helpers.intercept(wxb.navigateTo, navigateInterceptor)
-
-wxb.switchTab = helpers.intercept(wxb.switchTab, navigateInterceptor)
-
-wxb.redirectTo = helpers.intercept(wxb.redirectTo, navigateInterceptor)
-
-wxb.reLaunch = helpers.intercept(wxb.reLaunch, navigateInterceptor)
 
 wxb.showToast = helpers.intercept(wxb.showToast, {
   req (options) {
