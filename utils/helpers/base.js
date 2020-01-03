dd07033e8b34f008b4ace41c8a91f88db13fa572
zhaojintian <729234283@qq.com>
2020-01-03 10:10:25 +0800

    优化

--- utils/helpers/base.js
+++ utils/helpers/base.js
@@ -1,22 +1,3 @@
-import consts from '../consts'
 import helpers from 'jt-helpers'
 
-export default {
-  ...helpers,
-  getImageById (id) {
-    return `${consts.API_URL}/public/files/${id}`
-  },
-  getImageURL ({ id, width, height }) {
-    let sizeParams = ''
-
-    if (width && height) {
-      sizeParams = `?imageView2/1/w/${width}/h/${height}/q/100`
-    } else if (width) {
-      sizeParams = `?imageView2/2/w/${width}/q/100`
-    } else if (height) {
-      sizeParams = `?imageView2/2/h/${height}/q/100`
-    }
-
-    return `${consts.CDN_URL}/${id}${sizeParams}`
-  }
-}
+export default helpers
