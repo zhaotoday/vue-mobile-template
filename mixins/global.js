18519bb4b88825193e2ee01fdc47943eba5de42e
zhaojintian <729234283@qq.com>
2020-01-03 09:17:11 +0800

    update

--- mixins/global.js
+++ mixins/global.js
@@ -89,6 +89,22 @@ export default {
         ...list,
         items: list.items.filter(item => !!item[key])
       }
+    },
+    getImageById (id) {
+      return `${consts.API_URL}/public/files/${id}`
+    },
+    getImageURL ({ id, width, height }) {
+      let sizeParams = ''
+
+      if (width && height) {
+        sizeParams = `?imageView2/1/w/${width}/h/${height}/q/100`
+      } else if (width) {
+        sizeParams = `?imageView2/2/w/${width}/q/100`
+      } else if (height) {
+        sizeParams = `?imageView2/2/h/${height}/q/100`
+      }
+
+      return `${consts.CDN_URL}/${id}${sizeParams}`
     }
   }
 }
