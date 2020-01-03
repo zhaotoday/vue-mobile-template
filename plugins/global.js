dac7e82d6cb2493cd6db3b0ade32166b17645938
zhaojintian <729234283@qq.com>
2020-01-03 09:53:09 +0800

    update

--- plugins/global.js
+++ plugins/global.js
@@ -1,10 +1,10 @@
 import time from 'jt-time'
 import auth from 'we-design/utils/auth'
 import wxb from 'we-design/utils/wxb'
+import globalMixin from 'we-design/mixins/global'
 import store from '@/store'
 import consts from '@/utils/consts'
 import helpers from '@/utils/helpers/base'
-import globalMixin from '@/mixins/global'
 
 export default {
   install (Vue) {
