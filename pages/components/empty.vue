35a7fb2df309e0a6af141660ade2dab59d16ef29
zhaojintian <729234283@qq.com>
2020-01-04 23:02:19 +0800

    update

---
+++ pages/components/empty.vue
@@ -0,0 +1,7 @@
+<template>
+  <c-empty tip="暂无数据..."></c-empty>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/components/logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <c-logo></c-logo>
+</template>
+
+<script>
+export default {}
+</script>
