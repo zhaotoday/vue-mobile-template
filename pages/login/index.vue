f95e024f7fe64e3b2dc46b35cbd7c9cb945d0815
zhaojintian <729234283@qq.com>
2020-01-18 06:38:45 +0800

    update

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="p-login">
     <div class="b-logo">
-      <c-logo></c-logo>
+      <c-logo :name="$consts.NAME"></c-logo>
     </div>
-    <h2 class="b-title fs32">力软小程序将获取以下授权：</h2>
-    <p class="b-desc c31 fs28">获得您的公开信息（昵称、头像等）</p>
+    <h2 class="b-title fs32">{{ $consts.NAME }}将获取以下授权：</h2>
+    <p class="b-desc c37 fs28">获得您的公开信息（昵称、头像等）</p>
     <div class="b-buttons o-grid">
       <div class="o-grid__cell">
         <div
           class="c-button is-block w1 h76 bdc21 c21 fs32"
           style="margin-right: 20rpx;"
-          @click="navigateBack">
+          @click="navigateBack()">
           拒绝
         </div>
       </div>
       <div class="o-grid__cell">
+        <!-- #ifdef H5 -->
+        <button
+          class="c-button is-block w1 h76 bgc21 c11 fs32"
+          style="margin-left: 20rpx;"
+          @click="getUserInfo">
+          允许
+        </button>
+        <!-- #endif -->
+        <!-- #ifndef H5 -->
         <button
           class="c-button is-block w1 h76 bgc21 c11 fs32"
           open-type="getUserInfo"
@@ -22,6 +31,7 @@
           @getuserinfo="getUserInfo">
           允许
         </button>
+        <!-- #endif -->
       </div>
     </div>
   </div>
--- pages/login/script.js
+++ pages/login/script.js
@@ -1,9 +1,29 @@
 import CLogo from 'we-design/components/logo'
+import mockUser from 'we-design/utils/mock-user'
 
 export default {
   components: { CLogo },
   methods: {
+    async success () {
+      this.$wx.showToast({ title: '登录成功' })
+
+      await this.$helpers.sleep(1500)
+
+      if (!this.infoModified()) {
+        this.$wx.redirectTo({
+          url: this.$consts.INFO_PAGE
+        })
+      } else {
+        this.$wx.navigateBack()
+      }
+    },
     async getUserInfo () {
+      // #ifdef H5
+      mockUser.login()
+      await this.success()
+      // #endif
+
+      // #ifndef H5
       const getSettingRes = await this.$wx.getSetting()
 
       if (!getSettingRes.authSetting['scope.userInfo']) {
@@ -13,31 +33,24 @@ export default {
         const { iv, encryptedData } = await this.$wx.getUserInfo({
           withCredentials: true
         })
-        const wxUsersPostActionRes = await this.$store.dispatch('public/wxUsers/postAction', {
-          showLoading: true,
-          body: {
-            type: 'MP_GET_USER_INFO',
-            code: loginRes.code,
-            iv,
-            encryptedData,
-            loginRes
-          }
-        })
+        const wxUsersPostActionRes = await this.$store.dispatch(
+          'public/wxUsers/postAction', {
+            showLoading: true,
+            body: {
+              type: 'MP_GET_USER_INFO',
+              code: loginRes.code,
+              iv,
+              encryptedData,
+              loginRes
+            }
+          })
         const { wxUser, token } = wxUsersPostActionRes.data
 
         this.$auth.login({ user: wxUser, token })
-        this.$wx.showToast({ title: '登录成功' })
 
-        await this.$helpers.sleep(1500)
-
-        if (!this.infoModified()) {
-          this.$wx.redirectTo({
-            url: this.$consts.INFO_PAGE
-          })
-        } else {
-          this.$wx.navigateBack()
-        }
+        await this.success()
       }
+      // #endif
     }
   }
 }
