785658bb9fca82af514aa77d2778c1632cbac1cf
zhaojintian <729234283@qq.com>
2020-02-24 22:19:42 +0800

    update

--- pages/examples/comment/index.vue
+++ pages/examples/comment/index.vue
@@ -5,7 +5,7 @@
       class="c-comment__input fs28 bgc11"
       placholder-class="c32"
       placeholder="请输入您的姓名"
-      v-model="cForm.name"
+      v-model="cForm.model.name"
     />
     <h2 class="c-comment__title c12 fs30">手机号</h2>
     <input
@@ -14,14 +14,14 @@
       maxlength="11"
       placholder-class="c32"
       placeholder="请输入手机号"
-      v-model="cForm.phoneNumber"
+      v-model="cForm.model.phoneNumber"
     />
     <h2 class="c-comment__title c12 fs30">评论（可以不填）</h2>
     <textarea
       class="c-comment__textarea fs28 bgc11"
       placholder-class="c32"
       placeholder="请输入评论"
-      v-model="cForm.content"
+      v-model="cForm.model.content"
     >
     </textarea>
     <button class="c-button is-bottom w650 h88 bgc21 c11 fs32" @click="submit">
--- pages/examples/comment/script.js
+++ pages/examples/comment/script.js
@@ -4,12 +4,14 @@ import { Component } from "vue-property-decorator";
 @Component
 export default class CommentPage extends Vue {
   cForm = {
-    name: "",
-    phoneNumber: "",
-    content: ""
+    model: {
+      name: "",
+      phoneNumber: "",
+      content: ""
+    }
   };
 
   async submit() {
-    console.log("submit: ", this.cForm);
+    console.log("submit: ", this.cForm.model);
   }
 }
