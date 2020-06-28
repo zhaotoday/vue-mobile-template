9cfb1678033c7acbc5d05c7c1dabec2fd5e5a363
zhao <729234283@qq.com>
2020-06-28 18:30:50 +0800

    u

--- router/index.js
+++ router/index.js
@@ -1,12 +1,5 @@
 import Vue from "vue";
 import Router from "uni-simple-router";
-import auth from "we-design/utils/auth";
-import store from "@/store";
-import qs from "query-string";
-
-const LoginVersion = "v1.12";
-const { _, code, schoolId } = qs.parse(window.location.search);
-const page = window.location.hash.substr(1);
 
 Vue.use(Router);
 
@@ -16,30 +9,6 @@ const router = new Router({
 });
 
 router.beforeEach(async (to, from, next) => {
-  if (!auth.loggedIn({ code, version: LoginVersion })) {
-    const {
-      data: { redirectUrl, wxUser, token }
-    } = await store.dispatch("public/wxUsers/postAction", {
-      action: "login",
-      body: {
-        type: "Oa",
-        _,
-        code,
-        page,
-        extra: {
-          schoolId
-        }
-      }
-    });
-
-    if (redirectUrl) {
-      window.location.href = redirectUrl;
-    } else {
-      auth.login({ user: wxUser, token, code, version: LoginVersion });
-      store.dispatch("setUser", { user: auth.get()["user"] });
-    }
-  }
-
   next();
 });
 
