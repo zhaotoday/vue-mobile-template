0d5b024b4283d00a2b8c0b21f10207a1d02a9e4a
729234283 <729234283@qq.com>
2021-03-30 06:05:08 +0800

    update

--- pages/my/info/index.vue
+++ pages/my/info/index.vue
@@ -79,5 +79,3 @@
 </template>
 
 <script src="./script.js"></script>
-
-<style lang="scss" src="./style.scss"></style>
--- pages/my/phone-number/index.vue
+++ pages/my/phone-number/index.vue
@@ -36,5 +36,3 @@
 </template>
 
 <script src="./script.js"></script>
-
-<style lang="scss" src="./style.scss"></style>
