e6ebe1de2b2a7e9afb377cdec2b9749b8cb47286
zhaotoday <729234283@qq.com>
2021-05-28 10:26:17 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   globals: {
-    wx: true
+    wx: true,
+    uni: true,
+    ROUTES: true,
   },
   parserOptions: {
     parser: "babel-eslint",
     ecmaFeatures: {
-      legacyDecorators: true
-    }
+      legacyDecorators: true,
+    },
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
 };
