3c0e70a22f0fec085b2bfa7a36af757903a6f698
zhaotoday <729234283@qq.com>
2021-05-28 10:22:59 +0800

    update

---
+++ apis/wx/example.js
@@ -0,0 +1,13 @@
+import { Rest } from "vue-mobile/@lr/utils/rest";
+import { auth } from "vue-mobile/utils/auth";
+import { consts } from "@/utils/consts";
+
+export class ExampleApi extends Rest {
+  constructor() {
+    super();
+
+    this.baseUrl = consts.ApiUrl;
+    this.headers = auth.getHeaders();
+    this.path = "wx/example";
+  }
+}
