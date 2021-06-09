f782d1bdb64eb8658459206464eef4602e8ae13b
zhaotoday <729234283@qq.com>
2021-06-09 11:02:55 +0800

    调整目录结构

--- node_modules/vue-mobile/components/image/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div v-html="wrapHtml(html)"></div>
-</template>
-
-<script src="./script.js"></script>
--- node_modules/vue-mobile/components/more/script.js
+++
@@ -1,8 +0,0 @@
-export default {
-  props: {
-    text: {
-      type: String,
-      default: "查看更多",
-    },
-  },
-};
