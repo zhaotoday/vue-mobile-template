f8df390c2e893cc5526449b48ebb2f49883ea650
zhaotoday <729234283@qq.com>
2021-06-11 23:31:35 +0800

    update

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="p-home">home</div>
+  <div class="p-home u-m20">
+    <!-- c-avatar -->
+    <image
+      class="c-avatar r168"
+      mode="aspectFill"
+      src="https://img01.yzcdn.cn/vant/cat.jpeg"
+    />
+    <image
+      class="c-avatar w168 u-br8"
+      mode="aspectFill"
+      src="https://img01.yzcdn.cn/vant/cat.jpeg"
+    />
+    <div class="c-avatar c-avatar-default r168"></div>
+    <!-- button -->
+    <button class="c-button w650 h76 u-br8 bgc21 c11 fs32">允许</button>
+    <!-- icon -->
+    <i class="c-icon c-icon--logo"></i>
+  </div>
 </template>
 
 <script src="./script.js"></script>
