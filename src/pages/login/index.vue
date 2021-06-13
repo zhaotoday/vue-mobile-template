c368a2546d57e5183083c844f26300a27dfd7744
zhaotoday <729234283@qq.com>
2021-06-14 06:36:19 +0800

    update

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -5,33 +5,45 @@
     </div>
     <h2 class="b-title fs32">XXX 小程序将获取以下授权：</h2>
     <p class="b-desc t-gray7 fs28">获得您的公开信息（昵称、头像等）</p>
-    <div class="b-buttons o-grid">
-      <div class="o-grid__cell">
-        <div
-          class="c-button is-block h76 u-br8 bd-primary t-primary fs32 u-mr20"
-          @click="$wx.navigateBack()"
-        >
-          拒绝
+
+    <div class="b-buttons o-grid o-grid--12">
+      <div class="o-grid__row">
+        <div class="o-grid__col o-grid__col--6 p20">
+          <button
+            class="
+              c-button
+              is-block
+              h76
+              u-br8
+              bg-white
+              bd-primary
+              t-primary
+              fs32
+            "
+            @click="$wx.navigateBack()"
+          >
+            拒绝
+          </button>
         </div>
-      </div>
-      <div class="o-grid__cell">
-        <!-- #ifdef H5 -->
-        <button
-          class="c-button is-block h76 u-br8 bg-primary t-white fs32 u-ml20"
-          @click="getUserInfo"
-        >
-          允许
-        </button>
-        <!-- #endif -->
-        <!-- #ifndef H5 -->
-        <button
-          class="c-button is-block h76 u-br8 bg-primary t-white fs32 u-ml20"
-          open-type="getUserInfo"
-          @getuserinfo="getUserInfo"
-        >
-          允许
-        </button>
-        <!-- #endif -->
+        <div class="o-grid__col o-grid__col--6 p20">
+          <!-- #ifdef H5 -->
+          <button
+            class="c-button is-block h76 u-br8 bg-primary t-white fs32"
+            @click="getUserInfo"
+          >
+            允许
+          </button>
+          <!-- #endif -->
+          <!-- #ifndef H5 -->
+          <button
+            class="c-button is-block h76 u-br8 bg-primary t-white fs32"
+            open-type="getUserInfo"
+            @getuserinfo="getUserInfo"
+          >
+            允许
+          </button>
+          <!-- #endif -->
+        </div>
       </div>
     </div>
   </div>
