680c021492dab84ddbde92738fb53130ae592dbe
zhaojintian <729234283@qq.com>
2021-07-18 20:56:36 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import { onLaunch } from "uni-composition-api";
-import { useAuth } from "@/lr/composables/use-auth";
+import { useAuth } from "@lr/composables/use-auth";
 import { useEnums } from "@lr/composables/use-enums";
 import { useWxUser } from "@lr/composables/use-wx-user";
 
