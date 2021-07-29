43b0a1d2d74b05728b97f9b18ed2747160a098f7
zhaojintian <729234283@qq.com>
2021-07-29 15:48:09 +0800

    update

--- src/pages/user/login/index.vue
+++ src/pages/user/login/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="p-user-login">user-login</div>
+  <div class="p-login">
+    <div class="b-logo">
+      <c-logo
+        :name="$consts.SchoolConfig.name"
+        :css-classes="{ __icon: ['u-circle'] }"
+      />
+    </div>
+    <h2 class="b-title fs30">
+      {{ $consts.SchoolConfig.name }}将获取以下授权：
+    </h2>
+    <p class="b-desc t-gray7 fs28">获得您的公开信息（昵称、头像等）</p>
+    <div class="b-button">
+      <button
+        :class="bem.button.big.$"
+        class="is-block bg-primary t-white"
+        @click="login"
+      >
+        授权登录
+      </button>
+    </div>
+  </div>
 </template>
 
 <script src="./script.js"></script>
--- src/pages/user/login/script.js
+++ src/pages/user/login/script.js
@@ -1,1 +1,32 @@
-export default {};
+import wx from "wx-bridge";
+import { useBem } from "@/composables/use-bem";
+import { useWxUser } from "vue-mobile/@lr/composables/use-wx-user";
+import { useMockWxUser } from "vue-mobile/@lr/composables/use-mock-wx-user";
+
+export default {
+  setup() {
+    const bem = useBem();
+    const { appLogin } = useWxUser();
+
+    const login = async () => {
+      // #ifdef APP-PLUS
+      const {
+        authResult: { access_token: accessToken, openid: openId },
+      } = await wx.login({ provider: "weixin" });
+
+      await appLogin({ accessToken, openId });
+      // #endif
+
+      // #ifdef H5
+      await useMockWxUser().login();
+      // #endif
+
+      wx.switchTab({ url: "/pages/home/index" });
+    };
+
+    return {
+      bem,
+      login,
+    };
+  },
+};
