788230f79ecfe05dc228f5c8f7cb2657b97173df
zhaojintian <729234283@qq.com>
2021-07-29 15:42:54 +0800

    update

---
+++ src/pages/user/password/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-user-password">user-password</div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
---
+++ src/pages/user/register/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-user-register">user-register</div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
