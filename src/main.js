80f41ca6bec093387b68a24c7bcbf460887743f1
zhaojintian <729234283@qq.com>
2022-01-14 16:50:05 +0800

    u

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import {
-	createSSRApp
-} from "vue";
+import { createSSRApp } from "vue";
 import App from "./App.vue";
+import { store } from "./store";
+import { config } from "@/utils/config";
+import { useComponents } from "@/utils/use-components";
+
 export function createApp() {
-	const app = createSSRApp(App);
-	return {
-		app,
-	};
+  const app = createSSRApp(App);
+
+  app.use(store);
+
+  config(app);
+  useComponents(app);
+
+  return {
+    app,
+  };
 }
