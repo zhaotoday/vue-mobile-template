ac73fccd3ee0f2c27e5df1f98de24c3e2e682b97
zhaojintian <729234283@qq.com>
2022-01-17 21:24:06 +0800

    u

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script>
-import { useAuth } from "vue-mobile/@lr/composables/use-auth";
 import { useEnums } from "vue-mobile/@lr/composables/use-enums";
-import { useWxUser } from "vue-mobile/@lr/composables/use-wx-user";
 
 export default {
   async onLaunch() {
     const { getEnums } = useEnums();
-    const { getWxUser } = useWxUser();
 
     await getEnums();
-
-    if (useAuth().loggedIn()) await getWxUser();
   },
 };
 </script>
