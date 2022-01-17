89e297c4c97452c0b65e08dc648ced071abec572
zhaojintian <729234283@qq.com>
2022-01-17 18:08:29 +0800

    u

--- src/pages/login/script.js
+++ src/pages/login/script.js
@@ -1,21 +1,25 @@
 import wx from "wx-bridge";
 import { useHelpers } from "@/composables/use-helpers";
 import { useBem } from "@/composables/use-bem";
+import { useWxMp } from "vue-mobile/@lr/composables/use-wx-mp";
 import { useUsers } from "vue-mobile/@lr/composables/use-users";
 import { useMockUser } from "vue-mobile/@lr/composables/use-mock-user";
 
 export default {
   setup() {
     const bem = useBem();
+    const { getUserProfileAndLogin } = useWxMp();
+    const { wxMpLogin } = useUsers();
+    const { mockLogin } = useMockUser();
 
     const login = async () => {
       try {
         // #ifdef MP
-        await useUsers().wxMpLogin();
+        await wxMpLogin(await getUserProfileAndLogin());
         // #endif
 
         // #ifdef H5
-        await useMockUser().login();
+        await mockLogin();
         // #endif
 
         wx.showToast({ title: "登陆成功" });
