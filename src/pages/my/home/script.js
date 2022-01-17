f98b30001c3010f7296f4a96ed3ef277403fe804
zhaojintian <729234283@qq.com>
2022-01-17 18:11:03 +0800

    u

--- src/pages/my/home/script.js
+++ src/pages/my/home/script.js
@@ -1,9 +1,9 @@
-import { useWxUser } from "vue-mobile/@lr/composables/use-wx-user";
+import { useUsers } from "vue-mobile/@lr/composables/use-users";
 import avatarUrl from "vue-mobile/assets/images/components/avatar/default.png";
 
 export default {
   setup() {
-    const { wxUser, navigateTo } = useWxUser();
-    return { avatarUrl, wxUser, navigateTo };
+    const { user, navigateTo } = useUsers();
+    return { avatarUrl, user, navigateTo };
   },
 };
