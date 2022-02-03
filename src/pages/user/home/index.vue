821a5b642e9ed88e352cece65ef3c6f746d01854
zhaojintian <729234283@qq.com>
2022-02-03 23:05:33 +0800

    u

--- src/pages/user/home/index.vue
+++ src/pages/user/home/index.vue
@@ -1,34 +1,26 @@
 <template>
   <div class="p-user-home u-pt20">
-    <div
-      class="b-info bg-white u-mb20"
-      @click="$wx.navigateTo('/pages/user/info/index')"
-    >
+    <div class="b-info bg-white u-mb20">
       <image v-if="avatarUrl" class="b-info__avatar" :src="avatarUrl" />
       <image v-else class="b-info__avatar" :src="defaultAvatarUrl" />
       <div class="b-info__extra">
         <h2 class="fs30">{{ name }}</h2>
-      </div>
-      <div class="b-info__links">
-        <div class="c-tag h44 fs24 bgc21 c11">编辑资料</div>
       </div>
     </div>
     <div :class="$bem.box.$" class="u-mb20">
       <ul class="c-list bg-white fs28">
         <li
           class="c-list__item has-icon is-link"
+          @click="$wx.navigateTo('/pages/user/info/index')"
+        >
+          编辑资料
+        </li>
+        <li
+          class="c-list__item has-icon is-link"
           @click="$wx.navigateTo('/pages/user/account/home/index')"
         >
           <i class="c-iconfont c-iconfont--setup fs34"></i>
           账号设置
-        </li>
-        <li class="c-list__item has-icon is-link">
-          <i class="c-iconfont c-iconfont--help fs34"></i>
-          帮助文档
-        </li>
-        <li class="c-list__item has-icon is-link">
-          <i class="c-iconfont c-iconfont--letter fs34"></i>
-          问题需求反馈
         </li>
         <li class="c-list__item has-icon is-link" @click="logoutAndGotoLogin">
           <i class="c-iconfont c-iconfont--logout fs34"></i>
