183d385eb37b66c4018e1b4276490f8b2b2c911f
zhaojintian <729234283@qq.com>
2022-02-03 23:29:30 +0800

    u

--- src/pages/user/home/index.vue
+++ src/pages/user/home/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="p-user-home u-pt20">
     <div class="b-info bg-white u-mb20">
-      <image v-if="avatarUrl" class="b-info__avatar" :src="avatarUrl" />
-      <image v-else class="b-info__avatar" :src="defaultAvatarUrl" />
+      <u-avatar v-if="avatarUrl" size="120rpx" :src="avatarUrl" />
+      <u-avatar v-else size="120rpx" :src="defaultAvatarUrl" />
       <div class="b-info__extra">
-        <h2 class="fs30">{{ name }}</h2>
+        <h2 class="fs30">赵金添{{ name }}</h2>
+        <h3 class="fs26">13950442340</h3>
       </div>
     </div>
     <div :class="$bem.box.$" class="u-mb20">
@@ -13,19 +14,11 @@
           class="c-list__item has-icon is-link"
           @click="$wx.navigateTo('/pages/user/info/index')"
         >
-          编辑资料
+          修改用户信息
         </li>
-        <li
-          class="c-list__item has-icon is-link"
-          @click="$wx.navigateTo('/pages/user/account/home/index')"
-        >
-          <i class="c-iconfont c-iconfont--setup fs34"></i>
-          账号设置
-        </li>
-        <li class="c-list__item has-icon is-link" @click="logoutAndGotoLogin">
-          <i class="c-iconfont c-iconfont--logout fs34"></i>
-          退出账号
-        </li>
+        <li class="c-list__item has-icon is-link">修改密码</li>
+        <li class="c-list__item has-icon is-link">绑定手机号</li>
+        <li class="c-list__item has-icon is-link">绑定微信</li>
       </ul>
     </div>
   </div>
