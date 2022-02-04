56ce1ee8e8f0af4a289dfd8ed225e940300d360d
zhaojintian <729234283@qq.com>
2022-02-04 23:54:31 +0800

    u

--- src/plugins/global.js
+++ src/plugins/global.js
@@ -1,7 +1,5 @@
 import wx from "wx-bridge";
 import time from "jt-time";
-import { store } from "@/store";
-import { useAuth } from "vue-mobile/@lr/composables/use-auth";
 import { useConsts } from "@/composables/use-consts";
 import { useHelpers } from "@/composables/use-helpers";
 import { useBem } from "@/composables/use-bem";
@@ -10,18 +8,20 @@ export const globalPlugin = {
   install(Vue) {
     Vue.prototype.$wx = wx;
     Vue.prototype.$time = time;
-    Vue.prototype.$store = store;
-    Vue.prototype.$auth = useAuth();
     Vue.prototype.$consts = useConsts();
     Vue.prototype.$helpers = useHelpers();
     Vue.prototype.$bem = useBem();
 
+    // #ifdef MP
+    // BUGFIX
     Vue.mixin({
       data() {
         return {
+          $consts: useConsts(),
           $bem: useBem(),
         };
       },
     });
+    // #endif
   },
 };
