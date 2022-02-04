63720ec0170d1ec2d7a6729e17860598f654e31f
zhaojintian <729234283@qq.com>
2022-02-04 22:19:50 +0800

    u

--- src/pages/user/account-login/index.vue
+++ src/pages/user/account-login/index.vue
@@ -1,43 +1,36 @@
 <template>
   <div class="p-user-account-login u-pt18">
-    <div class="c-box">
-      <ul class="c-form c-form--inbox has-label u-mb40">
+    <div :class="$bem.box.$">
+      <ul class="c-form c-form--inbox has-label">
         <li class="c-form__item is-required">
-          <label class="fs32">姓名</label>
+          <label class="fs32">账号</label>
           <input
             class="c-form__input"
             placeholder-class="t-placeholder"
-            placeholder="请输入真实姓名"
-            v-model="cForm.model.nickName"
-            @blur="validate(cForm, 'nickName')"
+            placeholder="请输入手机号"
+            v-model="cForm.model.account"
+            @blur="validate(cForm, 'account')"
           />
-          <div v-if="cForm.errors.nickName" class="c-form__error">
-            {{ cForm.errors.nickName }}
+          <div v-if="cForm.errors.account" class="c-form__error">
+            {{ cForm.errors.account }}
+          </div>
+        </li>
+        <li class="c-form__item is-required">
+          <label class="fs32">密码</label>
+          <input
+            class="c-form__input"
+            type="password"
+            placeholder-class="t-placeholder"
+            placeholder="请输入密码"
+            v-model="cForm.model.password"
+            @blur="validate(cForm, 'password')"
+          />
+          <div v-if="cForm.errors.password" class="c-form__error">
+            {{ cForm.errors.password }}
           </div>
         </li>
       </ul>
     </div>
-    <ul class="c-list bg-white fs30">
-      <li class="c-list__item">
-        <label>账号</label>
-        <input
-          class="c-list__value"
-          placeholder-class="t-placeholder"
-          placeholder="请输入手机号"
-          v-model.trim="cForm.model.account"
-        />
-      </li>
-      <li class="c-list__item">
-        <label>密码</label>
-        <input
-          class="c-list__value"
-          type="password"
-          placeholder-class="t-placeholder"
-          placeholder="请输入密码"
-          v-model.trim="cForm.model.password"
-        />
-      </li>
-    </ul>
     <div class="u-tar u-pr10">
       <div
         class="b-password t-primary u-p20 fs26"
