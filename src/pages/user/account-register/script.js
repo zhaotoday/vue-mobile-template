b6f9f79526fe58a417eb186bf3cb41feb3f48b64
zhaojintian <729234283@qq.com>
2022-02-07 11:45:32 +0800

    u

--- src/pages/user/account-register/script.js
+++ src/pages/user/account-register/script.js
@@ -1,52 +1,38 @@
-import AsyncValidator from "async-validator";
-import { reactive } from "@vue/composition-api";
 import wx from "wx-bridge";
-import { useFormValidators } from "@lr/composables/use-form-validators";
+import { reactive } from "@vue/composition-api";
+import { useValidators } from "vue-validation";
 import { useHelpers } from "@/composables/use-helpers";
-import { useBem } from "@/composables/use-bem";
-import { useCaptcha } from "@lr/composables/use-captcha";
-import { PublicUsersApi } from "vue-mobile/@lr/apis/public/users";
-import { useUser } from "@lr/composables/use-user";
+import { useCaptcha } from "vue-mobile/@lr/composables/use-captcha";
+import { publicUsersApi } from "vue-mobile/@lr/apis/public/users";
 
 export default {
   setup() {
-    const bem = useBem();
-    const formValidators = useFormValidators();
-    const { accountRegister } = useUser();
+    const { isRequired, isPhoneNumber, isCaptcha, isPassword, validate } =
+      useValidators();
+
     const cForm = reactive({
       model: {},
       rules: {
-        nickName: [formValidators.required({ label: "昵称" })],
-        phoneNumber: [
-          formValidators.required({ label: "手机号" }),
-          formValidators.phoneNumber(),
-        ],
-        captcha: [
-          formValidators.required({ label: "验证码" }),
-          formValidators.captcha(),
-        ],
-        password: [
-          formValidators.required({ label: "密码" }),
-          formValidators.password(),
-        ],
+        phoneNumber: [isRequired({ label: "手机号" }), isPhoneNumber()],
+        captcha: [isRequired({ label: "验证码" }), isCaptcha()],
+        password: [isRequired({ label: "密码" }), isPassword()],
         confirmPassword: [
-          formValidators.required({ message: "请确认密码" }),
-          formValidators.password({ label: "确认密码" }),
+          isRequired({ message: "请确认密码" }),
+          isPassword({ label: "确认密码" }),
           {
-            validator(rule, value) {
-              return value === cForm.model.password;
-            },
+            validator: (rule, value) => value === cForm.model.password,
             message: "两次密码输入不一致",
           },
         ],
       },
+      errors: {},
     });
 
     const { cCaptcha, sendCaptcha } = useCaptcha({
       model: () => ({ phoneNumber: cForm.model.phoneNumber }),
       rules: () => ({ phoneNumber: cForm.rules.phoneNumber }),
       request: () =>
-        new PublicUsersApi().post({
+        publicUsersApi.post({
           action: "sendCaptcha",
           body: {
             phoneNumber: cForm.model.phoneNumber,
@@ -55,30 +41,33 @@ export default {
     });
 
     const submit = async () => {
-      const { rules, model } = cForm;
-      const { nickName, phoneNumber, captcha, password } = model;
+      await validate(
+        cForm,
+        null,
+        async (errors, { phoneNumber, captcha, password }) => {
+          if (errors) return;
 
-      await new AsyncValidator(rules).validate(model, async (errors) => {
-        if (errors) {
-          wx.showToast({ title: errors[0].message });
-          return;
+          await publicUsersApi.post({
+            action: "resetPassword",
+            body: {
+              phoneNumber,
+              captcha,
+              password,
+            },
+          });
+
+          wx.showToast({ title: "重置成功" });
+          await useHelpers().sleep(1500);
+          wx.navigateBack();
         }
-
-        await accountRegister({ nickName, phoneNumber, captcha, password });
-
-        wx.showToast({ title: "注册成功" });
-        await useHelpers().sleep(1500);
-        wx.navigateTo({
-          url: "/pages/phone-number/index?from=acount-register",
-        });
-      });
+      );
     };
 
     return {
-      bem,
       cForm,
       cCaptcha,
       sendCaptcha,
+      validate,
       submit,
     };
   },
