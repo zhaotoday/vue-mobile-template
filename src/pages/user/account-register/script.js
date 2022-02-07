4d87f359811205c02db1b70e4163e5027651045b
zhaojintian <729234283@qq.com>
2022-02-07 11:51:14 +0800

    u

--- src/pages/user/account-register/script.js
+++ src/pages/user/account-register/script.js
@@ -4,15 +4,19 @@ import { useValidators } from "vue-validation";
 import { useHelpers } from "@/composables/use-helpers";
 import { useCaptcha } from "vue-mobile/@lr/composables/use-captcha";
 import { publicUsersApi } from "vue-mobile/@lr/apis/public/users";
+import { useUsers } from "vue-mobile/@lr/composables/use-users";
 
 export default {
   setup() {
     const { isRequired, isPhoneNumber, isCaptcha, isPassword, validate } =
       useValidators();
 
+    const { accountRegister } = useUsers();
+
     const cForm = reactive({
       model: {},
       rules: {
+        nickName: [isRequired({ label: "昵称" })],
         phoneNumber: [isRequired({ label: "手机号" }), isPhoneNumber()],
         captcha: [isRequired({ label: "验证码" }), isCaptcha()],
         password: [isRequired({ label: "密码" }), isPassword()],
@@ -44,21 +48,16 @@ export default {
       await validate(
         cForm,
         null,
-        async (errors, { phoneNumber, captcha, password }) => {
+        async (errors, { nickName, phoneNumber, captcha, password }) => {
           if (errors) return;
 
-          await publicUsersApi.post({
-            action: "resetPassword",
-            body: {
-              phoneNumber,
-              captcha,
-              password,
-            },
-          });
+          await accountRegister({ nickName, phoneNumber, captcha, password });
 
-          wx.showToast({ title: "重置成功" });
+          wx.showToast({ title: "注册成功" });
           await useHelpers().sleep(1500);
-          wx.navigateBack();
+          wx.navigateTo({
+            url: "/pages/phone-number/index?from=acount-register",
+          });
         }
       );
     };
