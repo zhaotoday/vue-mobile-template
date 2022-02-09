ea64f985e73c1472dce222872d62a7da156ee22d
zhaojintian <729234283@qq.com>
2022-02-09 10:07:29 +0800

    u

---
+++ src/components/form/components/item/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="c-form__item">
+    <label>账号</label>
+    <input
+      class="c-form__input"
+      placeholder-class="t-placeholder"
+      placeholder="请输入手机号"
+      v-model.trim="cForm.model.account"
+      @blur="validate(cForm, 'account')"
+    />
+    <div v-if="cForm.errors.account" class="c-form__error">
+      {{ cForm.errors.account }}
+    </div>
+  </li>
+</template>
+
+<script src="./script.js"></script>
---
+++ src/components/form/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div :class="$bem.box.$">
+    <ul class="c-form c-form--inbox has-label">
+      <slot />
+    </ul>
+  </div>
+</template>
