e671d8419dba6fb2b2a117420c7ffba09986da48
zhaojintian <729234283@qq.com>
2022-02-09 17:03:31 +0800

    u

--- src/pages/user/password/index.vue
+++ src/pages/user/password/index.vue
@@ -1,75 +1,54 @@
 <template>
   <div class="p-user-phone-number u-pt30">
-    <div :class="$bem.box.$">
-      <ul class="c-form c-form--inbox has-label">
-        <li class="c-form__item">
-          <label>手机号</label>
-          <input
-            class="c-form__input"
-            type="number"
-            maxlength="11"
-            placeholder-class="t-placeholder"
-            placeholder="请输入手机号"
-            v-model="cForm.model.phoneNumber"
-            @blur="validate(cForm, 'phoneNumber')"
-          />
-          <div v-if="cForm.errors.phoneNumber" class="c-form__error">
-            {{ cForm.errors.phoneNumber }}
-          </div>
-        </li>
-        <li class="c-form__item">
-          <label>验证码</label>
-          <input
-            class="c-form__input"
-            type="number"
-            maxlength="6"
-            placeholder-class="t-placeholder"
-            placeholder="请输入验证码"
-            v-model.trim="cForm.model.captcha"
-            @blur="validate(cForm, 'captcha')"
-            style="width: 490rpx"
-          />
-          <div v-if="cForm.errors.captcha" class="c-form__error">
-            {{ cForm.errors.captcha }}
-          </div>
-          <button
-            class="c-form__send-captcha c-button h56 t-white fs26"
-            :class="cCaptcha.disabled ? 'bg-g4' : 'bg-primary'"
-            @click.stop="sendCaptcha"
-          >
-            {{ cCaptcha.message }}
-          </button>
-        </li>
-        <li class="c-form__item">
-          <label>密码</label>
-          <input
-            class="c-form__input"
-            type="password"
-            placeholder-class="t-placeholder"
-            placeholder="请输入密码（6-16位字母和数字组合）"
-            v-model="cForm.model.password"
-            @blur="validate(cForm, 'password')"
-          />
-          <div v-if="cForm.errors.password" class="c-form__error">
-            {{ cForm.errors.password }}
-          </div>
-        </li>
-        <li class="c-form__item">
-          <label>确认密码</label>
-          <input
-            class="c-form__input"
-            type="password"
-            placeholder-class="t-placeholder"
-            placeholder="请确认密码"
-            v-model="cForm.model.confirmPassword"
-            @blur="validate(cForm, 'confirmPassword')"
-          />
-          <div v-if="cForm.errors.confirmPassword" class="c-form__error">
-            {{ cForm.errors.confirmPassword }}
-          </div>
-        </li>
-      </ul>
-    </div>
+    <c-form>
+      <c-form-item label="手机号">
+        <c-form-input
+          type="number"
+          maxlength="11"
+          placeholder="请输入手机号"
+          v-model.trim="cForm.model.phoneNumber"
+          :error="cForm.errors.phoneNumber"
+          @blur="validate(cForm, 'phoneNumber')"
+        />
+      </c-form-item>
+      <c-form-item label="验证码">
+        <c-form-input
+          type="number"
+          maxlength="6"
+          placeholder="请输入验证码"
+          v-model.trim="cForm.model.captcha"
+          :error="cForm.errors.captcha"
+          custom-style="width: 300rpx"
+          @blur="validate(cForm, 'captcha')"
+        />
+        <button
+          slot="append"
+          class="c-form__send-captcha c-button h56 t-white fs26"
+          :class="cCaptcha.disabled ? 'bg-g4' : 'bg-primary'"
+          @click.stop="sendCaptcha"
+        >
+          {{ cCaptcha.message }}
+        </button>
+      </c-form-item>
+      <c-form-item label="密码">
+        <c-form-input
+          type="password"
+          placeholder="请输入密码（6-16位字母和数字组合）"
+          v-model.trim="cForm.model.password"
+          :error="cForm.errors.password"
+          @blur="validate(cForm, 'password')"
+        />
+      </c-form-item>
+      <c-form-item label="确认密码">
+        <c-form-input
+          type="password"
+          placeholder="请确认密码"
+          v-model.trim="cForm.model.confirmPassword"
+          :error="cForm.errors.confirmPassword"
+          @blur="validate(cForm, 'confirmPassword')"
+        />
+      </c-form-item>
+    </c-form>
     <u-button custom-class="at-bottom w690" type="primary" @click="submit">
       重置
     </u-button>
