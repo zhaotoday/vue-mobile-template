f402920f2e4df9e8ccbe8e44f01baab6a5c11d92
zhaojintian <729234283@qq.com>
2022-02-10 22:19:06 +0800

    u

--- src/pages/my/home/script.js
+++
@@ -1,9 +0,0 @@
-import { useUsers } from "vue-mobile/@lr/composables/use-users";
-import defaultAvatarUrl from "vue-mobile/assets/images/components/avatar/default.png";
-
-export default {
-  setup() {
-    const { user, name, avatarUrl, navigateTo } = useUsers();
-    return { defaultAvatarUrl, user, name, avatarUrl, navigateTo };
-  },
-};
