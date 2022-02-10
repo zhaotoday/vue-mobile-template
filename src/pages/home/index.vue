bc8cf77fa5ada27aa45b440ca39fc0943f8c924c
zhaojintian <729234283@qq.com>
2022-02-10 09:25:08 +0800

    u

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="p-home">
-    <div class="bg-primary u-p30">
+    <div class="bg-primary u-pl30 u-pr30 u-pt20 u-pb20">
       <u-search
         disabled
         :show-action="false"
         @click="$wx.navigateTo('/pages/user/account-register/index')"
+      />
+      <u-gap height="20rpx" />
+      <u-swiper
+        :list="swiperData"
+        height="300rpx"
+        indicator
+        indicator-mode="line"
       />
     </div>
   </div>
--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -1,3 +1,13 @@
 export default {
-  setup() {},
+  setup() {
+    const swiperData = [
+      "https://cdn.uviewui.com/uview/swiper/swiper1.png",
+      "https://cdn.uviewui.com/uview/swiper/swiper2.png",
+      "https://cdn.uviewui.com/uview/swiper/swiper3.png",
+    ];
+
+    return {
+      swiperData,
+    };
+  },
 };
