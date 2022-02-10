e809db87d125d7380299a9a0558f1ae2c6e6b14b
zhaojintian <729234283@qq.com>
2022-02-10 08:59:00 +0800

    u

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="p-home">home</div>
+  <div class="p-home">
+    <div class="bg-primary u-p30">
+      <u-search
+        disabled
+        :show-action="false"
+        @click="$wx.navigateTo('/pages/user/account-register/index')"
+      />
+    </div>
+  </div>
 </template>
 
 <script src="./script.js"></script>
