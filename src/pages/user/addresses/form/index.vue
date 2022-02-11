958d8096dd6452c287c879bf904e1772d16401fc
zhaojintian <729234283@qq.com>
2022-02-11 09:36:12 +0800

    u

--- src/pages/user/addresses/form/index.vue
+++ src/pages/user/addresses/form/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-user-addresses-form">
+  <div class="p-user-addresses-form u-pt30">
     <c-form>
       <c-form-item label="收货人">
         <c-form-input
@@ -13,10 +13,10 @@
             v-for="item in enums.UserAddressGender"
             :key="item.value"
             :class="[
-              'c-tag h50',
+              'c-tag h50 u-br10',
               cForm.model.gender === item.value
                 ? 'bd-primary t-primary'
-                : 'bd-g1 t-g1',
+                : 'bd-placeholder t-placeholder',
               'fs26',
             ]"
             @click="changeTag('gender', item)"
@@ -25,42 +25,27 @@
           </li>
         </ul>
       </c-form-item>
-    </c-form>
-    <ul class="c-list bgc11 fs30 u-mt20">
-      <li class="c-list__item">
-        收货人
-        <input
-          class="c-list__value fs32"
-          placeholder-class="c31"
-          placeholder="收货人姓名"
-          v-model="cForm.name"
-        />
-        <ul class="b-genders">
-          <li
-            v-for="item in $consts.GENDERS"
-            :key="item.value"
-            :class="[
-              'c-tag h50',
-              cForm.gender === item.value ? 'bdc22 c22' : 'bdc31 c31',
-              'fs26',
-            ]"
-            @click="changeTag('gender', item)"
-          >
-            {{ item.label }}
-          </li>
-        </ul>
-      </li>
-      <li class="c-list__item">
-        手机号
-        <input
-          class="c-list__value fs32"
+      <c-form-item label="手机号">
+        <c-form-input
           type="number"
           maxlength="11"
-          placeholder-class="c31"
           placeholder="配送员联系您的手机号"
-          v-model="cForm.phoneNumber"
+          v-model.trim="cForm.model.phoneNumber"
+          :error="cForm.errors.phoneNumber"
+          @blur="validate(cForm, 'phoneNumber')"
         />
-      </li>
+      </c-form-item>
+      <c-form-item custom-class="is-link" label="小区"> abc </c-form-item>
+      <c-form-item label="门牌号">
+        <c-form-input
+          placeholder="门牌号，如：1 号楼 102"
+          v-model.trim="cForm.model.room"
+          :error="cForm.errors.room"
+          @blur="validate(cForm, 'room')"
+        />
+      </c-form-item>
+    </c-form>
+    <ul v-if="false" class="c-list bgc11 fs30 u-mt20">
       <li class="c-list__item is-link" @click="selectLocation">
         小区
         <p v-if="cForm.location.name" class="c-list__value fs32">
