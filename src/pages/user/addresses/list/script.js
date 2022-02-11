81b2edcb66168d31a6de9b17fed991b5faa463bf
zhaojintian <729234283@qq.com>
2022-02-11 10:24:15 +0800

    i

--- src/pages/user/addresses/list/script.js
+++ src/pages/user/addresses/list/script.js
@@ -1,81 +1,79 @@
-import { mapState } from 'vuex'
-import CDialog from '@/components/dialog'
-import CEmpty from '@/components/empty'
+import { mapState } from "vuex";
 
 export default {
-  components: { CDialog, CEmpty },
-  data () {
+  setup() {},
+  data() {
     return {
       cDel: {
         visible: false,
-        id: 0
-      }
-    }
+        id: 0,
+      },
+    };
   },
   computed: mapState({
-    list: state => state['wx/addresses'].list
+    list: (state) => state["wx/addresses"].list,
   }),
-  async onShow () {
+  async onShow() {
     this.$wx.setNavigationBarTitle({
-      title: this.$mp.query.select ? '选择收货地址' : '收货地址'
-    })
+      title: this.$mp.query.select ? "选择收货地址" : "收货地址",
+    });
 
-    await this.getList()
+    await this.getList();
 
-    this.loaded = true
+    this.loaded = true;
   },
   methods: {
-    getList () {
-      return this.$store.dispatch('wx/addresses/getList', {
+    getList() {
+      return this.$store.dispatch("wx/addresses/getList", {
         query: {
           where: {
             wxUserId: {
-              $eq: this.$auth.get()['user'].id
-            }
-          }
-        }
-      })
+              $eq: this.$auth.get()["user"].id,
+            },
+          },
+        },
+      });
     },
-    async setDefault (item) {
-      await this.$store.dispatch('wx/addresses/postAction', {
+    async setDefault(item) {
+      await this.$store.dispatch("wx/addresses/postAction", {
         showLoading: true,
         body: {
-          type: 'SET_DEFAULT',
-          id: item.id
-        }
-      })
+          type: "SET_DEFAULT",
+          id: item.id,
+        },
+      });
 
       this.$wx.showToast({
-        title: '设置成功'
-      })
+        title: "设置成功",
+      });
 
-      this.getList()
+      this.getList();
     },
-    showDel (item) {
-      this.cDel.id = item.id
-      this.cDel.visible = true
+    showDel(item) {
+      this.cDel.id = item.id;
+      this.cDel.visible = true;
     },
-    async confirmDel () {
-      this.cDel.visible = false
+    async confirmDel() {
+      this.cDel.visible = false;
 
-      await this.$store.dispatch('wx/addresses/del', {
-        id: this.cDel.id
-      })
+      await this.$store.dispatch("wx/addresses/del", {
+        id: this.cDel.id,
+      });
 
       this.$wx.showToast({
-        title: '删除成功'
-      })
+        title: "删除成功",
+      });
 
-      this.getList()
+      this.getList();
     },
-    select (item) {
+    select(item) {
       if (this.$mp.query.select) {
-        this.$store.dispatch('wx/orders/setForm', {
-          key: 'address',
-          value: item
-        })
-        this.$wx.navigateBack()
+        this.$store.dispatch("wx/orders/setForm", {
+          key: "address",
+          value: item,
+        });
+        this.$wx.navigateBack();
       }
-    }
-  }
-}
+    },
+  },
+};
