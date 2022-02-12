ab746444a994ead4b24cfa4598176bc1550c67ee
zhaojintian <729234283@qq.com>
2022-02-13 00:31:23 +0800

    u

--- src/components/products/index.vue
+++ src/components/products/index.vue
@@ -1,21 +1,18 @@
 <template>
-  <u-grid
-    col="2"
-    :border="false"
-    style="width: 720rpx; margin: 0 auto"
-    class="c-products u-pt30 u-pb30"
-  >
-    <u-grid-item v-for="(item, index) in products" :key="index">
-      <div class="c-box bg-white u-br10 u-mb30">
-        <u-avatar shape="square" size="330rpx" :src="item.url" />
-        <div class="c-products__name fs28">{{ item.name }}</div>
-        <div class="c-products__price">
-          <span class="t-error fs26">¥100.0</span>
-          <span class="t-g6 u-lt fs24 u-ml10">200.0</span>
+  <div class="c-products u-pt30 u-pb30">
+    <u-grid col="2" :border="false">
+      <u-grid-item v-for="(item, index) in products" :key="index">
+        <div class="c-box bg-white u-br10 u-mb30">
+          <u-avatar shape="square" size="330rpx" :src="item.url" />
+          <div class="c-products__name fs28">{{ item.name }}</div>
+          <div class="c-products__price">
+            <span class="t-error fs26">¥100.0</span>
+            <span class="t-g6 u-lt fs24 u-ml10">200.0</span>
+          </div>
         </div>
-      </div>
-    </u-grid-item>
-  </u-grid>
+      </u-grid-item>
+    </u-grid>
+  </div>
 </template>
 
 <script src="./script.js"></script>
