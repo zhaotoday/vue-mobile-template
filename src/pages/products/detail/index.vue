ec2e7f3308189011770a15b1ca1295d6e3700f6a
zhaojintian <729234283@qq.com>
2022-02-13 10:37:36 +0800

    u

---
+++ src/pages/products/detail/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-products-detail">
+    <u-swiper
+      :list="swiperData"
+      height="750rpx"
+      indicator
+      radius="0"
+      indicator-mode="dot"
+    />
+    <div class="b-info">
+      <div class="b-info__name">
+        心相印纸巾抽纸整箱大包家用实惠装卫生纸心心相印餐巾纸面巾纸抽
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
---
+++ src/pages/products/detail/script.js
@@ -0,0 +1,13 @@
+export default {
+  setup() {
+    const swiperData = [
+      "https://cdn.uviewui.com/uview/swiper/swiper1.png",
+      "https://cdn.uviewui.com/uview/swiper/swiper2.png",
+      "https://cdn.uviewui.com/uview/swiper/swiper3.png",
+    ];
+
+    return {
+      swiperData,
+    };
+  },
+};
