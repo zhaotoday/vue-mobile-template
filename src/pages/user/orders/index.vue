212d43500c6fceb2d54e3dd06f835458f3e1a3d8
zhaojintian <729234283@qq.com>
2022-02-14 00:55:10 +0800

    i

--- src/pages/user/orders/index.vue
+++ src/pages/user/orders/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="p-orders u-pt30"></div>
+  <div class="p-orders">
+    <div class="bg-white u-pb10">
+      <u-tabs
+        :list="cTabs.items"
+        item-style="width: 25%; height: 80rpx; box-sizing: border-box"
+      />
+    </div>
+    <ul class="c-orders">
+      <li class="c-orders__item bg-white u-mt18">
+        <div class="c-orders__head">
+          <span class="fs26" style="padding-right: 20rpx">已完成</span>
+          <span class="t-gray7 fs24">2021</span>
+        </div>
+        <div class="c-orders__body o-media">
+          <img
+            class="o-media__image"
+            mode="aspectFill"
+            src="https://cdn.uviewui.com/uview/goods/1.jpg"
+          />
+          <div class="o-media__body">
+            <div class="cc-products">
+              <div>
+                <div class="cc-products__item c10 fs26">abc</div>
+              </div>
+            </div>
+            <div class="b-money fs26">实付 ￥12.00</div>
+          </div>
+        </div>
+        <div class="c-orders__foot u-tar">
+          <div class="c-tag h48 u-br8 bd-primary t-primary fs24">立即付款</div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script src="./script.js"></script>
