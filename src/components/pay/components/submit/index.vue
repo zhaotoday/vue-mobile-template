163c6df92d12195fc168ccb5f59e8eb5de7f9b69
zhaojintian <729234283@qq.com>
2022-02-14 00:34:32 +0800

    u

---
+++ src/components/pay/components/submit/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="cc-submit">
+    <div class="cc-submit__total fs28 t-error">¥123.00</div>
+    <div class="cc-submit__button bg-primary t-white fs28">支付</div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" scoped src="./style.scss"></style>
--- src/pages/pay/index.vue
+++ src/pages/pay/index.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="p-pay u-pt30">
-    <div :class="$bem.box.$">
-      <i class="c-iconfont c-iconfont--address fs40"></i>
-      <div class="c-address__head">
-        <h2 class="c-address__title fs28">淮安半岛</h2>
-        <p class="c-address__desc t-g7 fs24">赵金添 13950442340</p>
+    <div class="c-address u-mb30" :class="$bem.box.$">
+      <i
+        class="c-address__icon c-iconfont c-iconfont--address fs40 t-primary"
+      ></i>
+      <template v-if="false">
+        <div class="c-address__detail">
+          <h2 class="c-address__title fs28">淮安半岛</h2>
+          <p class="c-address__desc t-g7 fs24 u-pt16">赵金添 13950442340</p>
+        </div>
         <div class="c-tag h34 bd-primary t-primary u-br10 fs22">家</div>
+      </template>
+      <div v-else class="c-address__placeholder fs30 t-placeholder">
+        请选择收获地址
       </div>
     </div>
+    <div class="u-mb30" :class="$bem.box.$">
+      <div class="u-pt20 u-pb20 bg-white">
+        <div class="c-title c-title--md fs32">商品信息</div>
+      </div>
+      <gc-product-list :items="products" :col="1" />
+    </div>
+    <c-form class="u-mb30">
+      <c-form-item label="总计">
+        <div class="c-form__input fs30 t-error">¥123.00</div>
+      </c-form-item>
+      <c-form-item custom-class="is-link" label="支付方式">
+        <div class="c-form__input">货到付款</div>
+      </c-form-item>
+    </c-form>
+    <c-form>
+      <c-form-item label="备注">
+        <c-form-input placeholder="请输入备注信息" />
+      </c-form-item>
+    </c-form>
+    <gc-pay-submit />
   </div>
 </template>
 
