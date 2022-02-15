dbd75c1215f5b515c6a469c6ad618d4a623dcbb8
zhaojintian <729234283@qq.com>
2022-02-15 11:02:36 +0800

    u

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="p-cart u-pt24 u-pb100">
+    <u-empty
+      mode="car"
+      icon="http://cdn.uviewui.com/uview/empty/data.png"
+      margin-top="100rpx"
+    />
     <gc-product-list :items="products" :col="1" selectable />
     <gc-cart-submit />
   </div>
