fd63df890e8cf7a887c9cef1bb66a0d6385af2b5
zhaojintian <729234283@qq.com>
2022-02-15 10:15:00 +0800

    u

---
+++ src/pages/user/captcha-login/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="p-user-phone-number u-pt24">
+    <div
+      v-if="currentRoute.query.firstBind"
+      class="t-g7 fs26 u-pl30 u-pr30 u-pb20"
+      style="line-height: 150%"
+    >
+      应国家法律要求，使用互联网服务需进行账号实名。为保障账号的正常使用，请完成手机号绑定。感谢你的理解和支持。
+    </div>
+    <c-form>
+      <c-form-item label="手机号">
+        <c-form-input
+          type="number"
+          maxlength="11"
+          placeholder="请输入手机号"
+          v-model.trim="cForm.model.phoneNumber"
+          :error="cForm.errors.phoneNumber"
+          @blur="validate(cForm, 'phoneNumber')"
+        />
+      </c-form-item>
+      <c-form-item label="验证码">
+        <c-form-input
+          type="number"
+          maxlength="6"
+          placeholder="请输入验证码"
+          v-model.trim="cForm.model.captcha"
+          :error="cForm.errors.captcha"
+          custom-style="width: 300rpx"
+          @blur="validate(cForm, 'captcha')"
+        />
+        <button
+          slot="append"
+          class="c-form__send-captcha c-button h56 t-white fs26"
+          :class="cCaptcha.disabled ? 'bg-g4' : 'bg-primary'"
+          @click.stop="sendCaptcha"
+        >
+          {{ cCaptcha.message }}
+        </button>
+      </c-form-item>
+    </c-form>
+    <u-button custom-class="at-bottom w690" type="primary" @click="submit">
+      绑定
+    </u-button>
+  </div>
+</template>
+
+<script src="./script.js"></script>
---
+++ src/pages/user/captcha-login/script.js
@@ -0,0 +1,73 @@
+import wx from "wx-bridge";
+import { reactive } from "@vue/composition-api";
+import { useValidators } from "vue-validation";
+import { useHelpers } from "@/composables/use-helpers";
+import { useCaptcha } from "vue-mobile/@lr/composables/use-captcha";
+import { publicUsersApi } from "vue-mobile/@lr/apis/public/users";
+import { usersApi } from "vue-mobile/@lr/apis/client/users";
+import { useUsers } from "vue-mobile/@lr/composables/use-users";
+import { useRoute } from "vue-mobile/composables/use-route";
+
+export default {
+  setup() {
+    const { currentRoute } = useRoute();
+
+    const { isRequired, isPhoneNumber, isCaptcha, validate } = useValidators();
+
+    const { getUserInfo } = useUsers();
+
+    const cForm = reactive({
+      model: {},
+      rules: {
+        phoneNumber: [isRequired({ label: "手机号" }), isPhoneNumber()],
+        captcha: [isRequired({ label: "验证码" }), isCaptcha()],
+      },
+      errors: {},
+    });
+
+    const { cCaptcha, sendCaptcha } = useCaptcha({
+      model: () => ({ phoneNumber: cForm.model.phoneNumber }),
+      rules: () => ({ phoneNumber: cForm.rules.phoneNumber }),
+      request: () =>
+        publicUsersApi.post({
+          action: "sendSmsCaptcha",
+          body: {
+            phoneNumber: cForm.model.phoneNumber,
+          },
+        }),
+    });
+
+    const submit = async () => {
+      await validate(cForm, null, async (errors, { phoneNumber, captcha }) => {
+        if (errors) return;
+
+        await usersApi.post({
+          action: "phoneNumberBind",
+          body: {
+            phoneNumber,
+            captcha,
+          },
+        });
+
+        wx.showToast({ title: "绑定成功" });
+        await getUserInfo();
+        await useHelpers().sleep(1500);
+
+        if (currentRoute.query.firstBind) {
+          wx.switchTab({ url: "/pages/memo/index" });
+        } else {
+          wx.navigateBack();
+        }
+      });
+    };
+
+    return {
+      currentRoute,
+      cForm,
+      cCaptcha,
+      sendCaptcha,
+      validate,
+      submit,
+    };
+  },
+};
