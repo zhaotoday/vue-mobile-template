90998dafbd96f1075671d51c7b7f516209a5ab08
zhaojintian <729234283@qq.com>
2022-02-16 11:03:35 +0800

    u

---
+++ src/pages/manage/products/form/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="p-user-account-login u-pt24">
+    <c-form>
+      <c-form-item label="手机号">
+        <c-form-input
+          type="number"
+          maxlength="11"
+          placeholder="请输入手机号"
+          v-model.trim="cForm.model.account"
+          :error="cForm.errors.account"
+          @blur="validate(cForm, 'account')"
+        />
+      </c-form-item>
+      <c-form-item label="密码">
+        <c-form-input
+          type="password"
+          placeholder="请输入密码"
+          v-model.trim="cForm.model.password"
+          :error="cForm.errors.password"
+          @blur="validate(cForm, 'password')"
+        />
+      </c-form-item>
+    </c-form>
+    <div class="t-g7 fs28 u-p20">
+      <div
+        class="b-register"
+        @click="$wx.navigateTo('/pages/user/captcha-login/index')"
+      >
+        验证码登录
+      </div>
+      <div
+        class="b-password"
+        @click="$wx.navigateTo('/pages/user/password/index')"
+      >
+        忘记密码？
+      </div>
+    </div>
+    <u-button custom-class="at-bottom w702" type="primary" @click="submit">
+      登录
+    </u-button>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
--- src/pages/manage/products/form/script.js
+++ src/pages/manage/products/form/script.js
@@ -1,1 +1,43 @@
-export default {};
+import wx from "wx-bridge";
+import { reactive } from "@vue/composition-api";
+import { useValidators } from "vue-validation";
+import { useHelpers } from "@/composables/use-helpers";
+import { useUsers } from "vue-mobile/@lr/composables/use-users";
+import { onNavigationBarButtonTap } from "uni-composition-api";
+
+export default {
+  setup() {
+    const { isRequired, isPhoneNumber, isPassword, validate } = useValidators();
+    const { accountLogin } = useUsers();
+    const cForm = reactive({
+      model: {},
+      rules: {
+        account: [isRequired({ label: "手机号" }), isPhoneNumber()],
+        password: [isRequired({ label: "密码" }), isPassword()],
+      },
+      errors: {},
+    });
+
+    const submit = async () => {
+      await validate(cForm, null, async (errors, { account, password }) => {
+        if (errors) return;
+
+        await accountLogin({ account, password });
+
+        wx.showToast({ title: "登录成功" });
+        await useHelpers().sleep(1500);
+        wx.navigateTo({ url: "/pages/memo/index" });
+      });
+    };
+
+    onNavigationBarButtonTap(() => {
+      wx.navigateTo({ url: "/pages/user/account-register/index" });
+    });
+
+    return {
+      cForm,
+      validate,
+      submit,
+    };
+  },
+};
