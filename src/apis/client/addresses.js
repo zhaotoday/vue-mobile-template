be13e36f001caf6358681c33b0d70ae91b3bd644
zhaojintian <729234283@qq.com>
2022-02-20 01:08:23 +0800

    u

--- src/apis/client/addresses.js
+++ src/apis/client/addresses.js
@@ -3,5 +3,5 @@ import { useAuth } from "vue-mobile/@lr/composables/use-auth";
 
 export const addressesApi = createApi({
   url: "/client/addresses",
-  headers: useAuth().getHeaders(),
+  getHeaders: useAuth().getHeaders,
 });
