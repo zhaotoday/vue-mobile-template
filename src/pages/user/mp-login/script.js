3c8184d242090735615c7109ca52647604645148
zhaojintian <729234283@qq.com>
2022-02-20 09:01:03 +0800

    u

--- src/pages/user/mp-login/script.js
+++ src/pages/user/mp-login/script.js
@@ -3,12 +3,24 @@ import { useHelpers } from "@/composables/use-helpers";
 import { useWxMp } from "vue-mobile/@lr/composables/use-wx-mp";
 import { useUsers } from "vue-mobile/@lr/composables/use-users";
 import { useMockUser } from "vue-mobile/@lr/composables/use-mock-user";
+import { onHide, onShow } from "uni-composition-api";
 
 export default {
   setup() {
-    const { getUserProfileAndLogin } = useWxMp();
+    const { getUserProfileAndLogin, onRefreshLoginCode, offRefreshLoginCode } =
+      useWxMp();
     const { wxLogin } = useUsers();
     const { mockLogin } = useMockUser();
+
+    // #ifdefMP
+    onShow(async () => {
+      await onRefreshLoginCode();
+    });
+
+    onHide(() => {
+      offRefreshLoginCode();
+    });
+    // #endif
 
     const login = async () => {
       try {
