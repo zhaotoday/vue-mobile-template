ecb5677d07fa9063f2067e0a657d6c8db2b92544
zhaojintian <729234283@qq.com>
2022-02-27 19:01:23 +0800

    u

--- src/pages/manage/products/list/script.js
+++ src/pages/manage/products/list/script.js
@@ -1,19 +1,73 @@
-import products from "@/mock/products.json";
-import { reactive } from "@vue/composition-api";
+import { reactive, ref } from "@vue/composition-api";
+import { onShow } from "uni-composition-api";
+import { publicCategoriesApi } from "@/apis/public/catetgories";
+import { publicProductsApi } from "@/apis/public/products";
+import { usePageData } from "@/composables/use-page-data";
 
 export default {
   setup() {
+    const { setPageData, getCurrentPage, getPageData } = usePageData();
+
+    const loaded = ref(false);
+
     const cTab = reactive({
       current: 0,
     });
 
-    const changeCategory = (index) => {
+    const categoriesList = ref({
+      items: [],
+    });
+
+    const productsList = ref({
+      items: [],
+    });
+
+    onShow(async () => {
+      await renderCategoriesList();
+
+      cTab.current = getPageData({ page: getCurrentPage() }).currentIndex || 0;
+
+      if (categoriesList.value.items.length) {
+        await renderProductsList();
+      }
+
+      loaded.value = true;
+    });
+
+    const renderCategoriesList = async () => {
+      categoriesList.value = await publicCategoriesApi.get({
+        query: {
+          order: [["order", "DESC"]],
+        },
+      });
+    };
+
+    const renderProductsList = async () => {
+      productsList.value = await publicProductsApi.get({
+        query: {
+          where: {
+            categoryId: {
+              $eq: categoriesList.value.items[cTab.current].id,
+            },
+          },
+        },
+      });
+    };
+
+    const changeCategory = async (index) => {
       cTab.current = index;
+      setPageData({
+        page: getCurrentPage(),
+        data: { currentIndex: index },
+      });
+      await renderProductsList();
     };
 
     return {
-      products,
+      loaded,
       cTab,
+      categoriesList,
+      productsList,
       changeCategory,
     };
   },
