130e31b8ea7bcc9283b3a61cc4e9d68eb426f5c9
zhaojintian <729234283@qq.com>
2022-02-27 16:42:48 +0800

    u

--- src/pages/pay/result/index.vue
+++ src/pages/pay/result/index.vue
@@ -21,6 +21,4 @@
   </div>
 </template>
 
-<script src="./script.js"></script>
-
 <style lang="scss" src="./style.scss"></style>
--- src/pages/user/orders/detail/script.js
+++ src/pages/user/orders/detail/script.js
@@ -1,9 +1,20 @@
-import products from "@/mock/products.json";
+import { ref } from "@vue/composition-api";
+import { onShow } from "uni-composition-api";
+import { ordersApi } from "@/apis/client/orders";
+import { useRoute } from "vue-mobile/composables/use-route";
 
 export default {
   setup() {
+    const { currentRoute } = useRoute();
+
+    const detail = ref({});
+
+    onShow(async () => {
+      detail.value = await ordersApi.get({ id: currentRoute.query.id });
+    });
+
     return {
-      products,
+      detail,
     };
   },
 };
