ee74c832b6497ce5aaea3a0d9afe94f9f2de1d08
zhaojintian <729234283@qq.com>
2022-02-27 16:57:38 +0800

    u

--- src/pages/user/orders/detail/index.vue
+++ src/pages/user/orders/detail/index.vue
@@ -5,13 +5,19 @@
         <div class="c-form__input">12456461132</div>
       </c-form-item>
       <c-form-item label="下单时间">
-        <div class="c-form__input">2022-02-16 00:00</div>
+        <div class="c-form__input">{{ $time.getTime(detail.createdAt) }}</div>
       </c-form-item>
       <c-form-item label="送达时间">
         <div class="c-form__input">2022-02-16 00:00</div>
       </c-form-item>
       <c-form-item label="状态">
-        <div class="c-form__input">已完成</div>
+        <div class="c-form__input">
+          {{
+            $helpers.getItem(enums.OrderStatus, "value", detail.status)[
+              "label"
+            ] || ""
+          }}
+        </div>
       </c-form-item>
     </c-form>
     <div class="c-address u-mb24" :class="$bem.box.$">
@@ -19,8 +25,12 @@
         class="c-address__icon c-iconfont c-iconfont--address fs44 t-primary"
       ></i>
       <div class="c-address__detail">
-        <h2 class="c-address__title fs28">淮安半岛</h2>
-        <p class="c-address__desc t-g7 fs24 u-pt16">赵金添 13950442340</p>
+        <h2 class="c-address__title fs28">
+          {{ detail.address.location.name }}
+        </h2>
+        <p class="c-address__desc t-g7 fs24 u-pt16">
+          {{ detail.address.name }} {{ detail.address.phoneNumber }}
+        </p>
       </div>
       <div class="c-tag h34 bd-primary t-primary u-br10 fs22">家</div>
     </div>
@@ -29,7 +39,7 @@
         <div class="c-title c-title--md fs32">商品信息</div>
       </div>
       <gc-product-list
-        :items="products"
+        :items="detail.products"
         :col="1"
         :edit-number="false"
         show-number
@@ -37,7 +47,9 @@
     </div>
     <c-form custom-class="u-mb24">
       <c-form-item label="总计">
-        <div class="c-form__input fs30 t-error">¥123.00</div>
+        <div class="c-form__input fs30 t-error">
+          ¥{{ getTotalPrice(detail.products) }}
+        </div>
       </c-form-item>
       <c-form-item label="支付方式">
         <div class="c-form__input">货到付款</div>
@@ -45,7 +57,7 @@
     </c-form>
     <c-form>
       <c-form-item label="备注">
-        <div class="c-form__input">备注信息</div>
+        <div class="c-form__input">{{ detail.remark }}</div>
       </c-form-item>
     </c-form>
   </div>
--- src/pages/user/orders/detail/script.js
+++ src/pages/user/orders/detail/script.js
@@ -2,19 +2,37 @@ import { ref } from "@vue/composition-api";
 import { onShow } from "uni-composition-api";
 import { ordersApi } from "@/apis/client/orders";
 import { useRoute } from "vue-mobile/composables/use-route";
+import { useProducts } from "@/composables/use-products";
+import { useEnums } from "vue-mobile/@lr/composables/use-enums";
 
 export default {
   setup() {
+    const { enums } = useEnums();
+
     const { currentRoute } = useRoute();
 
-    const detail = ref({});
+    const { getTotalPrice } = useProducts();
+
+    const detail = ref({
+      products: [],
+      address: {
+        location: {},
+      },
+    });
 
     onShow(async () => {
-      detail.value = await ordersApi.get({ id: currentRoute.query.id });
+      detail.value = await ordersApi.get({
+        id: currentRoute.query.id,
+        query: {
+          include: [{ model: "Address", as: "address" }],
+        },
+      });
     });
 
     return {
+      enums,
       detail,
+      getTotalPrice,
     };
   },
 };
