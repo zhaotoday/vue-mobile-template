374862808d8e1d01b4ac3656110663b6f35e51ca
zhaojintian <729234283@qq.com>
2022-03-05 01:03:28 +0800

    u

--- src/pages/user/account-login/script.js
+++ src/pages/user/account-login/script.js
@@ -9,16 +9,19 @@ import { useI18n } from "@/composables/use-i18n";
 export default {
   setup() {
     const { t } = useI18n();
-
-    console.log(t("abc", { aa: 2 }));
-
     const { isRequired, isPhoneNumber, isPassword, validate } = useValidators();
     const { accountLogin } = useUsers();
     const cForm = reactive({
       model: {},
       rules: {
-        account: [isRequired({ label: "手机号" }), isPhoneNumber()],
-        password: [isRequired({ label: "密码" }), isPassword()],
+        account: [
+          isRequired({ message: t("$g.input.phoneNumber") }),
+          isPhoneNumber(),
+        ],
+        password: [
+          isRequired({ message: t("$g.input.password") }),
+          isPassword(),
+        ],
       },
       errors: {},
     });
