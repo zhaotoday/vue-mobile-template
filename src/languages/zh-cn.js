4a4e7873c03f7c27096714e51dbde4be71707001
zhaojintian <729234283@qq.com>
2022-03-05 10:32:21 +0800

    u

--- src/languages/zh-cn.js
+++ src/languages/zh-cn.js
@@ -1,84 +1,74 @@
 export const zhCn = {
-  $g: {
-    $t: {
-      remark: "备注",
-      stocks: "库存",
-      sales: "销量",
-      details: "详情",
-      name: "姓名",
-      phoneNumber: "手机号",
-      password: "密码",
-      confirmPassword: "确认密码",
-      captcha: "验证码",
-      login: "登录",
-      register: "注册",
-      save: "保存",
-    },
-    inputs: {
-      content: "请输入内容",
-      productName: "请输入商品名称",
-      name: "请输入姓名",
-      phoneNumber: "请输入手机号",
-      password: "请输入密码",
-      confirmPassword: "请确认密码",
-      passwordTip: "请输入密码（6-16位字母和数字组合）",
-      captcha: "请输入验证码",
-      confirmPasswordNotEqualPassword: "两次密码输入不一致",
-    },
-    tips: {
-      loginSuccess: "登录成功",
-      registerSuccess: "注册成功",
-    },
+  titles: {
+    remark: "备注",
+    stocks: "库存",
+    sales: "销量",
+    details: "详情",
+    name: "姓名",
+    phoneNumber: "手机号",
+    password: "密码",
+    confirmPassword: "确认密码",
+    captcha: "验证码",
+    login: "登录",
+    register: "注册",
+    save: "保存",
     colon: "：",
   },
-  $p: {
-    pay: {
-      $p: {
-        result: {
-          paySuccess: "下单成功",
-          gotoOrders: "查看订单列表",
-          backHome: "返回首页",
+  inputs: {
+    content: "请输入内容",
+    productName: "请输入商品名称",
+    name: "请输入姓名",
+    phoneNumber: "请输入手机号",
+    password: "请输入密码",
+    confirmPassword: "请确认密码",
+    passwordTip: "请输入密码（6-16位字母和数字组合）",
+    captcha: "请输入验证码",
+    confirmPasswordNotEqualPassword: "两次密码输入不一致",
+  },
+  tips: {
+    loginSuccess: "登录成功",
+    registerSuccess: "注册成功",
+  },
+  pay: {
+    result: {
+      paySuccess: "下单成功",
+      gotoOrders: "查看订单列表",
+      backHome: "返回首页",
+    },
+    selectAddress: "请选择收获地址",
+    productInfo: "商品信息",
+    total: "总计",
+    payment: "支付方式",
+    cashOnDelivery: "货到付款",
+  },
+  products: {
+    search: {
+      history: "历史搜索",
+      hot: "热门搜索",
+    },
+  },
+  user: {
+    accountLogin: {
+      captchaLogin: "验证码登录",
+      forgotPassword: "忘记密码？",
+    },
+    addresses: {
+      $g: {
+        $t: {
+          consignee: "收货人",
+          housingEstate: "小区",
+          houseNo: "门牌号",
+          addressTag: "地址类型",
         },
       },
-      selectAddress: "请选择收获地址",
-      productInfo: "商品信息",
-      total: "总计",
-      payment: "支付方式",
-      cashOnDelivery: "货到付款",
-    },
-    products: {
-      $p: {
-        search: {
-          history: "历史搜索",
-          hot: "热门搜索",
-        },
+      inputs: {
+        consignee: "收货人姓名",
+        byWhichPhoneWeContactYou: "配送员联系您的手机号",
+        housingEstate: "小区/写字楼等",
+        houseNo: "门牌号，如：1 号楼 102",
       },
-    },
-    user: {
       $p: {
-        accountLogin: {
-          captchaLogin: "验证码登录",
-          forgotPassword: "忘记密码？",
-        },
-        addresses: {
-          $g: {
-            $t: {
-              consignee: "收货人",
-              housingEstate: "小区",
-              houseNo: "门牌号",
-              addressTag: "地址类型",
-            },
-          },
-          inputs: {
-            consignee: "收货人姓名",
-            byWhichPhoneWeContactYou: "配送员联系您的手机号",
-            housingEstate: "小区/写字楼等",
-            houseNo: "门牌号，如：1 号楼 102",
-          },
-          $p: {
-            form: {},
-          },
-        },
+        form: {},
       },
     },
   },
