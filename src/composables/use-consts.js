31796f78787be4cfcabe7951b9c8990803377e7f
zhaojintian <729234283@qq.com>
2022-03-07 23:45:18 +0800

    u

--- src/composables/use-consts.js
+++ src/composables/use-consts.js
@@ -1,12 +1,14 @@
 export const useConsts = () => {
   // 接口地址
-  const ApiUrl = "http://localhost:10601";
+  // const ApiUrl = "http://localhost:10601";
+  const ApiUrl = "https://sol-api.fzlr.com";
 
   // 登录页面地址
   const LoginUrl = "/pages/user/account-login/index";
 
   // CDN 地址
-  const CdnUrl = "http://localhost:10601/public/files";
+  // const CdnUrl = "http://localhost:10601/public/files";
+  const CdnUrl = "https://sol-api.fzlr.com/public/files";
 
   // 静态资源地址
   const StaticUrl = "https://my-app.liruan.cn/static";
