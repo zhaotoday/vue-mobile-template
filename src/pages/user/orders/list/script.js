dd856d34780fe2b7413329f6627602fac3b05481
zhaojintian <729234283@qq.com>
2022-03-07 16:26:22 +0800

    u

--- src/pages/user/orders/list/script.js
+++ src/pages/user/orders/list/script.js
@@ -1,59 +1,20 @@
-import { computed, ref } from "@vue/composition-api";
 import { onShow } from "uni-composition-api";
-import { ordersApi } from "@/apis/client/orders";
-import { useUsers } from "vue-mobile/@lr/composables/use-users";
 import { useProducts } from "@/composables/use-products";
-import { useI18n } from "@/composables/use-i18n";
 import { useEnums } from "vue-mobile/@lr/composables/use-enums";
+import { useRender } from "./composables/use-render";
 
 export default {
   setup() {
-    const { $t } = useI18n({ path: "user/orders/list" });
-
     const { enums } = useEnums();
-
-    const { user } = useUsers();
 
     const { getTotalPrice } = useProducts();
 
-    const loaded = ref(false);
-
-    const cTabs = {
-      current: 0,
-    };
-
-    const tabItems = computed(() => [
-      {
-        label: $t("$.orderStatuses.all"),
-        value: "",
-      },
-      ...enums.value.OrderStatus,
-    ]);
-
-    const list = ref({
-      items: [],
-    });
+    const { loaded, cTabs, tabItems, list, render, changeTab } = useRender();
 
     onShow(async () => {
       await render();
       loaded.value = true;
     });
-
-    const render = async () => {
-      list.value = await ordersApi.get({
-        query: {
-          where: {
-            userId: { $eq: user.value.id },
-            status: { $eq: tabItems.value[cTabs.current].value },
-          },
-        },
-      });
-    };
-
-    const changeTab = async ({ index }) => {
-      cTabs.current = index;
-      await render();
-    };
 
     return {
       enums,
