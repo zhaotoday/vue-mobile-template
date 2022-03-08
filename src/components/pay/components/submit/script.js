17f8bf824dd81977cd7755a9823599a5c7e28c03
zhaojintian <729234283@qq.com>
2022-03-08 09:01:02 +0800

    i

--- src/components/pay/components/submit/script.js
+++ src/components/pay/components/submit/script.js
@@ -1,3 +1,15 @@
+import { useCart } from "@/composables/use-cart";
+import { useProducts } from "@/composables/use-products";
+
 export default {
   emits: ["submit"],
+  setup() {
+    const { selectedProducts } = useCart();
+    const { getTotalPrice } = useProducts();
+
+    return {
+      selectedProducts,
+      getTotalPrice,
+    };
+  },
 };
