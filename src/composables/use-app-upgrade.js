d9f69ca621b3cb3409d2b6d02eee2599e520d6a9
zhaojintian <729234283@qq.com>
2022-03-09 23:48:22 +0800

    update

--- src/composables/use-app-upgrade.js
+++ src/composables/use-app-upgrade.js
@@ -1,1 +1,16 @@
-export const useAppUpgrade = () => {};
+import { publicAppUpgradesApi } from "@/apis/public/app-upgrades";
+
+export const useAppUpgrade = () => {
+  const check = async () => {
+    // #ifdef APP-PLUS
+    const a = await publicAppUpgradesApi.post({
+      action: "check",
+    });
+    console.log(a);
+    // #endif
+  };
+
+  return {
+    check,
+  };
+};
