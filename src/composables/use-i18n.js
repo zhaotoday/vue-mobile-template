52c5c06109fa04a492b60e20cb484c498baf5e65
zhaojintian <729234283@qq.com>
2022-03-12 20:55:07 +0800

    u

--- src/composables/use-i18n.js
+++ src/composables/use-i18n.js
@@ -1,17 +1,17 @@
 import { getCurrentInstance } from "@vue/composition-api";
 
-export const useI18n = ({ path }) => {
+export const useI18n = ({ page = "", component = "" }) => {
   const vm = getCurrentInstance();
-
-  const pathSplit = path
-    .split("/")
-    .map((item) => item.replace(/-/g, "_").toUpperCase());
 
   const $t = (key, locale) => {
     return vm.proxy.$i18n.t(key, locale);
   };
 
-  const formatKey = (key) => {
+  const formatKey = (path, key) => {
+    const pathSplit = path
+      .split("/")
+      .map((item) => item.replace(/-/g, "_").toUpperCase());
+
     return key.substr(0, 2) === "_."
       ? pathSplit
           .filter((item, index) => index < pathSplit.length - 1)
@@ -22,10 +22,10 @@ export const useI18n = ({ path }) => {
   };
 
   const pt = (key, locale) =>
-    vm.proxy.$i18n.t("pages." + formatKey(key), locale);
+    vm.proxy.$i18n.t("pages." + formatKey(page, key), locale);
 
   const ct = (key, locale) =>
-    vm.proxy.$i18n.t("components." + formatKey(key), locale);
+    vm.proxy.$i18n.t("components." + formatKey(component, key), locale);
 
   return {
     $t,
