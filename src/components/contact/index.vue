bc88ac80d2b74f3f617daadc2d607f6916e90591
zhaojintian <729234283@qq.com>
2022-03-21 15:04:52 +0800

    u

--- src/components/contact/index.vue
+++ src/components/contact/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="c-contact bg-secondary">
+  <div
+    class="c-contact bg-secondary"
+    @click="$wx.makePhoneCall({ phoneNumber: '7207858' })"
+  >
     <i class="c-iconfont c-iconfont--telephone t-white fs40"></i>
   </div>
 </template>
