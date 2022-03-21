a54c9648a017e0de3b37ea4112d132cdd7c578cf
zhaojintian <729234283@qq.com>
2022-03-21 14:47:16 +0800

    u

---
+++ src/components/contact/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="c-contact">
+    <i class="c-iconfont c-iconfont--phone"></i>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
