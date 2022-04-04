9a4d01282043adc4a7ce4196746b41cb4d2fb53f
zhaojintian <729234283@qq.com>
2022-04-04 23:54:23 +0800

    u

--- src/pages/im/chats/detail/index.vue
+++ src/pages/im/chats/detail/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="p-im-chats-list">im chats list</div>
+  <div class="p-chat-message-window">
+    <scroll-view
+      class="b-chat-messages"
+      scroll-y
+      :scroll-into-view="lastViewId"
+    >
+      <c-chat-messages :items="list.items" />
+    </scroll-view>
+  </div>
 </template>
 
 <script src="./script.js"></script>
--- src/pages/im/chats/detail/script.js
+++ src/pages/im/chats/detail/script.js
@@ -1,3 +1,48 @@
+import ChatMessages from "@/components/chat/messages";
+import { onShow } from "uni-composition-api";
+import { useCurrentRoute } from "vue-mobile/composables/use-current-route";
+import { MessagesApi } from "@/apis/wx/messages";
+import { ref } from "@vue/composition-api";
+import { useChat } from "@/components/chat/composables/use-chat";
+import { useHelpers } from "@/composables/use-helpers";
+import wx from "wx-bridge";
+
 export default {
-  setup() {},
+  components: {
+    "c-chat-messages": ChatMessages,
+  },
+  setup() {
+    const { currentRoute } = useCurrentRoute();
+
+    const { formatMessages } = useChat();
+
+    const list = ref({
+      items: [],
+    });
+
+    const lastViewId = ref("");
+
+    onShow(async () => {
+      const { chatId, name } = currentRoute.query;
+
+      wx.setNavigationBarTitle({ title: name });
+
+      const { items } = await new MessagesApi().get({
+        query: { chatId },
+      });
+
+      list.value = { items: formatMessages(items) };
+
+      await useHelpers().sleep(100);
+
+      if (items.length) {
+        lastViewId.value = `message-${items[items.length - 1].id}`;
+      }
+    });
+
+    return {
+      list,
+      lastViewId,
+    };
+  },
 };
