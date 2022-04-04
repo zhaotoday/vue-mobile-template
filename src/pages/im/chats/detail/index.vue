1543aca08cc8b4ae32c89a1a731d84f9feeeecb7
zhaojintian <729234283@qq.com>
2022-04-04 22:50:35 +0800

    u

---
+++ src/pages/im/chats/detail/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-im-chats-list">im chats list</div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
---
+++ src/pages/im/chats/list/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-im-chats-detail">im chats list</div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
