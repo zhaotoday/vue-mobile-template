c1174a6a7c739a3c3a0c6f7d7fdd678215f170d0
zhaojintian <729234283@qq.com>
2022-04-04 22:57:28 +0800

    u

--- src/pages/im/chats/list/index.vue
+++ src/pages/im/chats/list/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="p-im-chats-detail">im chats list</div>
+  <div>
+    <div class="bg-white u-pt16 u-pb16">
+      <c-search placeholder="请输入好友名称" @confirm="confirmSearch" />
+    </div>
+    <c-chat-list :items="filteredItems" />
+    <c-empty v-if="loaded && !filteredItems.length" />
+  </div>
 </template>
 
 <script src="./script.js"></script>
-
-<style lang="scss" src="./style.scss"></style>
--- src/pages/im/chats/list/script.js
+++ src/pages/im/chats/list/script.js
@@ -1,3 +1,59 @@
+import { useChat } from "@/components/chat/composables/use-chat";
+import { reactive, ref } from "@vue/composition-api";
+import ChatList from "@/components/chat/list";
+
 export default {
-  setup() {},
+  components: {
+    "c-chat-list": ChatList,
+  },
+  computed: {
+    filteredItems() {
+      return this.list.items.filter((item) =>
+        item.name.includes(this.cSearch.keywords)
+      );
+    },
+  },
+  setup() {
+    const loaded = ref(false);
+
+    const { ws, formatChatList, getChatList } = useChat();
+
+    const cSearch = reactive({
+      keywords: "",
+    });
+
+    const list = ref({
+      items: [],
+    });
+
+    const onGetChatList = (data) => {
+      list.value = { items: formatChatList(data) };
+      loaded.value = true;
+    };
+
+    const render = async () => {
+      ws.on(ws.events.getChatList, onGetChatList);
+
+      ws.ready(() => {
+        getChatList();
+      });
+    };
+
+    const unRender = () => {
+      ws.off(ws.events.getChatList, onGetChatList);
+    };
+
+    const confirmSearch = (value) => {
+      cSearch.keywords = value;
+    };
+
+    return {
+      loaded,
+      cSearch,
+      list,
+      render,
+      unRender,
+      confirmSearch,
+    };
+  },
 };
