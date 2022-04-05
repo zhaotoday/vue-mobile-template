850b82226093d2e5becb65e54933cabc9efa1a6a
zhaojintian <729234283@qq.com>
2022-04-05 16:50:19 +0800

    i

--- src/components/contact/index.vue
+++ src/components/contact/index.vue
@@ -1,10 +1,18 @@
 <template>
-  <div
-    class="c-contact bg-secondary"
-    @click="$wx.makePhoneCall({ phoneNumber: '7204466' })"
-  >
-    <i class="c-iconfont c-iconfont--telephone t-white fs40"></i>
-  </div>
+  <ul class="c-contact">
+    <li
+      class="c-contact bg-secondary"
+      @click="$wx.makePhoneCall({ phoneNumber: '7204466' })"
+    >
+      <i class="c-iconfont c-iconfont--telephone t-white fs40"></i>
+    </li>
+    <li
+      class="c-contact bg-secondary"
+      @click="$wx.makePhoneCall({ phoneNumber: '7204466' })"
+    >
+      <i class="c-iconfont c-iconfont--telephone t-white fs40"></i>
+    </li>
+  </ul>
 </template>
 
 <script src="./script.js"></script>
