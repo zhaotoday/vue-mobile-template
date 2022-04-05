49e35a705254259821dce4a8421be515966c1dd8
zhaojintian <729234283@qq.com>
2022-04-05 16:15:22 +0800

    u

--- src/composables/use-consts.js
+++ src/composables/use-consts.js
@@ -3,7 +3,7 @@ import { Languages } from "@/utils/consts/languages";
 export const useConsts = () => {
   // 接口地址
   // const ApiUrl = "http://localhost:10601";
-  const ApiUrl = "http://sol-api.fzlr.com";
+  const ApiUrl = "https://sol-api.fzlr.com";
 
   // 登录页面地址
   const LoginUrl = "/pages/user/account-login/index";
