f8e9414c18c1fa5ad560642bf07658475db5b290
zhaojintian <729234283@qq.com>
2022-04-27 22:13:27 +0800

    u

--- src/apis/client/im-messages.js
+++
@@ -1,7 +0,0 @@
-import { createApi } from "vue-mobile/@lr/utils/create-api";
-import { useAuth } from "vue-mobile/@lr/composables/use-auth";
-
-export const imMessagesApi = createApi({
-  url: "/client/imMessages",
-  getHeaders: useAuth().getHeaders,
-});
