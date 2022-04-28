1129ad3db5fa894c460b0b7b3f6a2325630ef142
zhaojintian <729234283@qq.com>
2022-04-28 09:57:08 +0800

    u

--- src/pages/user/home/script.js
+++ src/pages/user/home/script.js
@@ -2,15 +2,12 @@ import defaultAvatarUrl from "vue-mobile/assets/images/components/avatar/default
 import { useUsers } from "vue-mobile/@lr/composables/use-users";
 import { onShow } from "uni-composition-api";
 import wx from "wx-bridge";
-import { useI18n } from "vue-mobile/composables/use-i18n";
 import { store } from "@/store";
 import { useTabBarBadge } from "@/composables/use-tab-bar-badge";
 import mainMenus from "./utils/main-menus.json";
 
 export default {
   setup() {
-    const { pt, $t } = useI18n({ page: "user/home" });
-
     const { loggedIn, user, getUserInfo, navigateTo, name, avatarUrl, logout } =
       useUsers();
 
@@ -22,21 +19,20 @@ export default {
 
     const logoutAndGotoHome = async () => {
       const { confirm } = await wx.showModal({
-        title: $t("tips.pleaseConfirm"),
-        content: pt("tips.confirmLogout"),
-        confirmText: $t("$.logout"),
+        title: "请确认",
+        content: "确认退出账号吗？",
+        confirmText: "退出账号",
       });
 
       if (confirm) {
         await logout();
-        wx.showToast({ title: $t("tips.logoutSuccess") });
+        wx.showToast({ title: "退出成功" });
       }
     };
 
     const wxMpBind = () => {};
 
     return {
-      pt,
       defaultAvatarUrl,
       loggedIn,
       user,
