70b75cf10bfbe9747c1f90890c8e07ff2c5edc83
zhaojintian <729234283@qq.com>
2023-04-12 11:42:32 +0800

    u

--- src/composables/use-consts.js
+++ src/composables/use-consts.js
@@ -2,27 +2,27 @@ export const useConsts = () => {
   // 接口地址
   const API_URL = process.env["VUE_APP_API_URL"];
 
-  // 登录页面地址
-  const LOGIN_URL = "/pages/user/account-login/index";
+  // 静态资源地址
+  const StaticUrl = process.env["VUE_APP_STATIC_URL"];
 
   // CDN 地址
   const CDN_URL = process.env["VUE_APP_CDN_URL"];
 
-  // 静态资源地址
-  const StaticUrl = process.env["VUE_APP_STATIC_URL"];
-
   // WebSocket 地址
   const WsUrl = process.env["VUE_APP_WS_URL"];
+
+  // 登录页面地址
+  const LOGIN_URL = "/pages/user/account-login/index";
 
   // 购物车 tabBar 的索引
   const CartTabBarIndex = 2;
 
   return {
     API_URL,
-    LOGIN_URL,
-    CDN_URL,
     StaticUrl,
+    CDN_URL,
     WsUrl,
+    LOGIN_URL,
     CartTabBarIndex,
   };
 };
