1bb910bf165eab64b8824f16a2353ecd7e2362f1
zhaojintian <729234283@qq.com>
2023-04-24 07:26:58 +0800

    update

--- src/utils/use-composition-api.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import VueCompositionAPI from "@vue/composition-api";
-import UniCompositionAPI from "uni-composition-api";
-
-Vue.use(VueCompositionAPI);
-Vue.use(UniCompositionAPI);
